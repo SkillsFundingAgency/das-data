ea365aa9e57c94d238e97261d46688c7fba3d0c5
51monfarrow <simon@mostlymonkey.co.uk>
2016-12-15 10:46:26 +0000

    first pass at schedulted job

--- src/SFA.DAS.Data.AccountBalance/WorkerRole.cs
+++ src/SFA.DAS.Data.AccountBalance/WorkerRole.cs
@@ -1,23 +1,46 @@
 using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.Storage;
+using Quartz;
+using Quartz.Impl;
+using SFA.DAS.Data.Pipeline.Helpers;
 using SFA.DAS.EAS.Account.Api.Client;
+using SFA.DAS.EAS.Account.Api.Client.Dtos;
+using Simple.Data;
 
 namespace SFA.DAS.Data.AccountBalance
 {
+    public class AccountBalanceJob : EntityListPoll<AccountWithBalanceViewModel, AccountWithBalanceViewModel>
+    {
+        public override void Configure(EntityListPoll<AccountWithBalanceViewModel, AccountWithBalanceViewModel> cfg)
+        {
+            var configuration = new AccountApiConfiguration
+            {
+                ApiBaseUrl = ConfigurationManager.AppSettings["ApiBaseUrl"],
+                ClientSecret = ConfigurationManager.AppSettings["ClientSecret"]
+            };
+            var client = new AccountApiClient(configuration);
+            var source = new ApiWrapper(client);
+
+            //this will need a connection string etc.
+            var db = Database.Open();
+            var conn = new DbWrapper {Wrapper = db};
+
+            //still need to set up logging
+            cfg.SetSource(source.GetAccounts)
+                .BuildPipeline(v => v.Store(conn, "balance"));
+        }
+    }
+
     public class WorkerRole : RoleEntryPoint
     {
         private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+        private IScheduler sched;
 
         public override void Run()
         {
@@ -35,42 +58,41 @@ namespace SFA.DAS.Data.AccountBalance
 
         public override bool OnStart()
         {
-            // Set the maximum number of concurrent connections
-            ServicePointManager.DefaultConnectionLimit = 12;
+            ISchedulerFactory sf = new StdSchedulerFactory();
+            sched = sf.GetScheduler();
 
-            // For information on handling configuration changes
-            // see the MSDN topic at https://go.microsoft.com/fwlink/?LinkId=166357.
+            LoadJobs(sched);
 
-            bool result = base.OnStart();
+            sched.Start();
 
-            Trace.TraceInformation("SFA.DAS.Data.AccountBalance has been started");
+            return base.OnStart();
+        }
 
-            return result;
+        public static void LoadJobs(IScheduler sched)
+        {
+            var job = JobBuilder.Create<AccountBalanceJob>()
+                .WithIdentity("job1", "group1")
+                .Build();
+
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .ForJob(job)
+                .StartNow()
+                .WithSimpleSchedule(x => x.RepeatForever().WithIntervalInHours(1))
+                .Build();
+
+            sched.ScheduleJob(job, trigger);
         }
 
         public override void OnStop()
         {
-            Trace.TraceInformation("SFA.DAS.Data.AccountBalance is stopping");
-
-            this.cancellationTokenSource.Cancel();
-            this.runCompleteEvent.WaitOne();
-
+            sched.Shutdown(false);
+            sched = null;
             base.OnStop();
-
-            Trace.TraceInformation("SFA.DAS.Data.AccountBalance has stopped");
         }
 
         private async Task RunAsync(CancellationToken cancellationToken)
         {
-            var configuration = new AccountApiConfiguration
-            {
-                ApiBaseUrl = ConfigurationManager.AppSettings["ApiBaseUrl"],
-                ClientSecret = ConfigurationManager.AppSettings["ClientSecret"]
-            };
-            var client = new AccountApiClient(configuration);
-            var source = new ApiWrapper(client);
-
-
             // TODO: Replace the following with your own logic.
             while (!cancellationToken.IsCancellationRequested)
             {
@@ -80,40 +102,3 @@ namespace SFA.DAS.Data.AccountBalance
         }
     }
 }
-
-
-//public override bool OnStart()
-//{
-//    ISchedulerFactory sf = new StdSchedulerFactory();
-//    var sched = sf.GetScheduler();
-
-//    LoadJobs(sched);
-
-//    sched.Start();
-
-//    return base.OnStart();
-//}
-
-//public static void LoadJobs(IScheduler sched)
-//{
-//    IJobDetail job = JobBuilder.Create()
-//                    .WithIdentity("job1", "group1")
-//                    .Build();
-
-//    ITrigger trigger = TriggerBuilder.Create<DatabaseMaintenanceJob>()
-//        .WithIdentity("trigger1", "group1")
-//        .ForJob(job)
-//        .StartNow()
-//        .WithSimpleSchedule(x => x.RepeatForever().WithIntervalInHours(1))
-//        .Build();
-
-//    sched.ScheduleJob(job, trigger);
-//}
-
-//class DatabaseMaintenanceJob : IJob
-//{
-//    public void Execute(IJobExecutionContext context)
-//    {
-//        // Code to do the db maintenance here
-//    }
-//}
