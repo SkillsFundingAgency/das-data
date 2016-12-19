ec63877715323c9d052a8f177468f73cbafe112d
51monfarrow <simon@mostlymonkey.co.uk>
2016-12-19 15:07:19 +0000

    added test for api wrapper

---
+++ src/SFA.DAS.Data.AccountBalance.UnitTests/TestApiWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.EAS.Account.Api.Client;
+using SFA.DAS.EAS.Account.Api.Client.Dtos;
+
+namespace SFA.DAS.Data.AccountBalance.UnitTests
+{
+    [TestFixture]
+    public class TestApiWrapper
+    {
+        public class FakeAccountApi : IAccountApiClient
+        {
+            private PagedApiResponseViewModel<AccountWithBalanceViewModel> res(int page)
+            {
+                return new PagedApiResponseViewModel<AccountWithBalanceViewModel>
+                {
+                    Data = Enumerable.Range(0, 10)
+                        .Select(i => new AccountWithBalanceViewModel {AccountHashId = i.ToString()}).ToList(),
+                    Page = 0,
+                    TotalPages = 3 - page
+                };
+            }
+
+            public Task<PagedApiResponseViewModel<AccountWithBalanceViewModel>> GetPageOfAccounts(int pageNumber = 1, int pageSize = 1000, DateTime? toDate = null)
+            {
+                return Task.Run(() => res(pageNumber));
+            }
+        }
+
+
+        [Test]
+        public void CanPage()
+        {
+            var fake = new FakeAccountApi();
+
+            var wrapper = new ApiWrapper(fake);
+
+            var reults = wrapper.GetAccounts().ToList();
+        }
+    }
+}
