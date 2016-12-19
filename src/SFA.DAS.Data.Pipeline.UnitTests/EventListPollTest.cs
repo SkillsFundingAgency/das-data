51bf42d1506fadd47977db57287a855ca318e960
51monfarrow <simon@mostlymonkey.co.uk>
2016-12-19 14:43:47 +0000

    added test for single entity poller

--- src/SFA.DAS.Data.Pipeline.UnitTests/EventListPollTest.cs
+++ src/SFA.DAS.Data.Pipeline.UnitTests/EventListPollTest.cs
@@ -44,4 +44,40 @@ namespace SFA.DAS.Data.Pipeline.UnitTests
             Assert.AreEqual("Hi bob",job.Output.First());
         }
     }
+
+    [TestFixture]
+    public class EntityPollTest
+    {
+        public class One : EntityPoll<string, string>
+        {
+            public LogToList Logger = new LogToList();
+            public List<string> Output = new List<string>();
+
+            public override void Configure(EntityPoll<string, string> cfg)
+            {
+                cfg
+                    .SetSource(() => "bob")
+                    .SetLog(Logger.Log)
+                    .BuildPipeline(
+                        r => r
+                            .Step(s => Result.Win("Hi " + s, "not much")
+                            .Step(s2 =>
+                            {
+                                Output.Add(s2);
+                                return Result.Win(s2, "stashed");
+                            })));
+            }
+        }
+
+        [Test]
+        public void ProcessList()
+        {
+            var mock = new Mock<IJobExecutionContext>();
+            var job = new One();
+            job.Execute(mock.Object);
+
+            Assert.AreEqual(1, job.Output.Count);
+            Assert.AreEqual("Hi bob", job.Output.First());
+        }
+    }
 }
