0faa632a2a0bb44b55a689390072ad2e51261896
Michael Young <mick.young1981@googlemail.com>
2017-01-10 11:04:00 +0000

    Added legal entity id to employer registrations view.

--- src/SFA.DAS.Data.Database/Views/DAS_Employer_Registrations.sql
+++ src/SFA.DAS.Data.Database/Views/DAS_Employer_Registrations.sql
@@ -13,6 +13,7 @@ SELECT
 	   ,LegalEntityNumber
 	   ,LegalOrganisatioCompanyReferenceNumber = CASE WHEN LegalEntitySource = 'Companies House' THEN LegalEntityNumber ELSE '' END
 	   ,LegalOrganisatioCharityCommissionNumber = CASE WHEN LegalEntitySource = 'Charity Commission' THEN LegalEntityNumber ELSE '' END
-       ,'Suppressed' AS OwnerEmail -- Supressed as not in data processing agreement
+       ,'Suppressed' AS OwnerEmail -- Supressed as not in data processing agreement,
+	   ID AS LegalEntityId
 FROM Data_Load.DAS_Employer_Registrations
 
