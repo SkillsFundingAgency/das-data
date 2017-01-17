88973a8fa3792e34552f82fb5336f6a87bf65db6
Michael Young <mick.young1981@googlemail.com>
2017-01-17 08:04:25 +0000

    Updated registrations view with new version provided by Stuart.

--- src/SFA.DAS.Data.Database/Views/DAS_Employer_Registrations.sql
+++ src/SFA.DAS.Data.Database/Views/DAS_Employer_Registrations.sql
@@ -1,19 +1,23 @@
-﻿CREATE VIEW [Data_Pub].DAS_Employer_Registrations
+﻿CREATE VIEW [Data_Pub].[DAS_Employer_Registrations]
 AS
-SELECT
-       ID    
-       ,DasAccountId
-       ,DASAccountName
-       ,DateRegistered
-       ,LegalEntityName
-       ,'Suppressed' AS LegalEntityRegisteredAddress  -- Supressed as not in data processing agreement
-       ,LegalEntitySource
-	   ,LegalEntityStatus
-       ,LegalEntityCreatedDate
-	   ,LegalEntityNumber
-	   ,LegalOrganisatioCompanyReferenceNumber = CASE WHEN LegalEntitySource = 'Companies House' THEN LegalEntityNumber ELSE '' END
-	   ,LegalOrganisatioCharityCommissionNumber = CASE WHEN LegalEntitySource = 'Charity Commission' THEN LegalEntityNumber ELSE '' END
-       ,'Suppressed' AS OwnerEmail, -- Supressed as not in data processing agreement,
-	   ID AS LegalEntityId
-FROM Data_Load.DAS_Employer_Registrations
-
+SELECT  [Id]
+      ,A.[DasAccountId]
+      ,A.[DasAccountName]
+      ,A.[DateRegistered]
+      ,A.[LegalEntityRegisteredAddress]
+      ,A.[LegalEntitySource]
+      ,A.[LegalEntityStatus]
+      ,A.[LegalEntityName]
+      ,A.[LegalEntityCreatedDate]
+      ,A.[LegalEntityNumber]
+          , CASE WHEN A.LegalEntitySource = 'Companies House' THEN A.LegalEntityNumber ELSE '' END AS LegalOrganisatioCompanyReferenceNumber
+          , CASE WHEN A.LegalEntitySource = 'Charity Commission' THEN A.LegalEntityNumber ELSE '' END AS LegalOrganisatioCharityCommissionNumber
+       ,'Suppressed' AS OwnerEmail -- Supressed as not in data processing agreement,
+      ,A.[LegalEntityId]
+      ,A.[UpdateDateTime]
+         ,CASE WHEN B.Flag_Latest = 1 THEN 1 ELSE 0 END AS Flag_Latest
+  FROM [Data_Load].[DAS_Employer_Registrations] AS A
+       LEFT JOIN (   SELECT DASAccountID, LegalEntityName, MAX([UpdateDateTime]) AS Max_UpdateDateTime, 1 AS Flag_Latest
+                           FROM [Data_Load].[DAS_Employer_Registrations]
+                           GROUP BY DASAccountID, LegalEntityName  
+                            ) AS B ON A.DASAccountID = B.DASAccountID AND A.LegalEntityName = B.LegalEntityName AND A.UpdateDateTime = B.Max_UpdateDateTime
