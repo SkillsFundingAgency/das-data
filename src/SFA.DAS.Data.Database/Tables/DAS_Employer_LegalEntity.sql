159f6c83e489966921dcd6bbb854bafb87c75719
Michael Young <mick.young1981@googlemail.com>
2017-01-30 10:46:45 +0000

    Added new data structure for accounts.

---
+++ src/SFA.DAS.Data.Database/Tables/DAS_Employer_LegalEntity.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Data_Load].[DAS_Employer_LegalEntity]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [DasAccountId] NVARCHAR(100) NOT NULL, 
+    [DasLegalEntityId] NCHAR(10) NOT NULL, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Address] NVARCHAR(256) NULL, 
+    [Source] NVARCHAR(50) NOT NULL, 
+    [InceptionDate] DATETIME NULL, 
+    [Code] NVARCHAR(50) NULL, 
+    [Status] NVARCHAR(50) NULL, 
+    [UpdateDate] DATETIME NOT NULL DEFAULT (GETDATE())
+)
