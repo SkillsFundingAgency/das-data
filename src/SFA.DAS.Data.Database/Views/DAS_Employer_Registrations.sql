5150eb6cce1447d24e31ee23cd8995c3e2853207
Michael Young <mick.young1981@googlemail.com>
2017-02-01 14:39:51 +0000

    Changed registrations view to work with the new data structure.

--- src/SFA.DAS.Data.Database/Views/DAS_Employer_Registrations.sql
+++ src/SFA.DAS.Data.Database/Views/DAS_Employer_Registrations.sql
@@ -1,69 +1,69 @@
 ﻿CREATE VIEW [Data_Pub].[DAS_Employer_Registrations]
 AS
-      SELECT [A].[Id] AS Row_ID
+	SELECT ROW_NUMBER() OVER (ORDER BY [A].ID ASC, [LE].ID) AS Row_ID
           , [A].[DasAccountId]
-          , [A].[DasAccountName]
+          , [A].[AccountName] AS DASAccountName
           , CONVERT(DATE,[A].[DateRegistered]) AS [DateRegistered]
 		  , [A].[DateRegistered] AS [DateTimeRegistered]
-		  , [A].[LegalEntityID] AS [LegalEntityId]
-		  , [A].[LegalEntityName]
-          , [A].[LegalEntityRegisteredAddress]
-          , [A].[LegalEntitySource]
-          , [A].[LegalEntityStatus]
-          , [A].[LegalEntityCreatedDate]
-          , [A].[LegalEntityNumber]
+		  , [LE].[DasLegalEntityID] AS [LegalEntityId]
+		  , [LE].[Name] AS LegalEntityName
+          , [LE].[Address] AS LegalEntityRegisteredAddress
+          , [LE].[Source] AS LegalEntitySource
+          , [LE].[Status] AS LegalEntityStatus
+          , [LE].[InceptionDate] AS LegalEntityCreatedDate
+          , [LE].[Code] AS LegalEntityNumber
           , CASE
-                WHEN [A].[LegalEntitySource] = 'Companies House'
-                THEN [A].[LegalEntityNumber]
+                WHEN [LE].[Source] = 'Companies House'
+                THEN [LE].[Code]
                 ELSE ''
             END AS [LegalOrganisatioCompanyReferenceNumber]
           , CASE
-                WHEN [A].[LegalEntitySource] = 'Charity Commission'
-                THEN [A].[LegalEntityNumber]
+                WHEN [LE].[Source] = 'Charity Commission'
+                THEN [LE].[Code]
                 ELSE ''
             END AS [LegalOrganisatioCharityCommissionNumber]
           , 'Suppressed' AS [OwnerEmail] -- Supressed as not in data processing agreement,
          -- , ROW_NUMBER() OVER(ORDER BY [A].[LegalEntityName] ASC) AS [LegalEntityId]
-                , [A].[PayeSchemeName]
+          , [paye].[Name] AS PayeSchemeName
           , [A].[UpdateDateTime]
-          , CASE
-                WHEN [B].[Flag_Latest] = 1
-                THEN 1
-                ELSE 0
-            END AS [Flag_Latest]
+          , CASE WHEN [B].[Flag_Latest] = 1 THEN 1 ELSE 0 END AS Flag_Latest 
           , CASE
                      -- Other also flag to Red
-                WHEN [A].[LegalEntitySource] = 'Other' THEN 'Red'
+                WHEN [LE].[Source] = 'Other' THEN 'Red'
                      --Charity's commission always flag to Green
-                WHEN [A].[LegalEntitySource] = 'Charities' THEN
-                           (CASE WHEN [A].[LegalEntityNumber] IS NULL OR [A].[LegalEntityNumber] = '0' THEN 'Red' ELSE 'Green' END)
+                WHEN [LE].[Source] = 'Charities' THEN
+                           (CASE WHEN [LE].[Code] IS NULL OR [LE].[Code] = '0' THEN 'Red' ELSE 'Green' END)
                      --When company if first to charactors are text then flag as Amber else green
-                      WHEN [A].[LegalEntitySource] = 'Companies House' THEN
-                           (CASE WHEN [A].[LegalEntityNumber] IS NULL OR [A].[LegalEntityNumber] = '0' THEN 'Red'
-                                                WHEN isnumeric(left([A].[LegalEntityNumber],2)) <> 1 THEN 'Amber' ELSE 'Green' END)
+                      WHEN [LE].[Source] = 'Companies House' THEN
+                           (CASE WHEN [LE].[Code] IS NULL OR [LE].[Code] = '0' THEN 'Red'
+                                                WHEN isnumeric(left([LE].[Code],2)) <> 1 THEN 'Amber' ELSE 'Green' END)
                      -- Public Sector always set to Amber
-                     WHEN [A].[LegalEntitySource] = 'Public Bodies' THEN 'Amber'             
+                     WHEN [LE].[Code] = 'Public Bodies' THEN 'Amber'             
                       ELSE 'ERROR'
             END AS [LegalEntityRAGRating]
-     ,  CASE WHEN [A].[LegalEntitySource] IN ('Charities','Companies House') THEN
-          (CASE WHEN [A].[LegalEntityNumber] IS NULL OR [A].[LegalEntityNumber] = '0' THEN [A].[LegalEntityName] ELSE CAST([A].[LegalEntityNumber] AS VARCHAR(255)) END)
-                ELSE [A].[LegalEntityName] END AS  UniqueLegalEntitID
+     ,  CASE WHEN [LE].[Source] IN ('Charities','Companies House') THEN
+          (CASE WHEN [LE].[Code] IS NULL OR [LE].[Code] = '0' THEN [LE].[Name] ELSE CAST([LE].[Code] AS VARCHAR(255)) END)
+                ELSE [LE].[Name] END AS  UniqueLegalEntitID
       
        FROM
-        Data_Load.DAS_Employer_Registrations AS A
+        Data_Load.DAS_Employer_Accounts AS A
+		INNER JOIN Data_Load.DAS_Employer_LegalEntities AS LE ON LE.DasAccountId = A.DasAccountId
+		INNER JOIN Data_Load.DAS_Employer_PayeSchemes AS paye ON paye.DasAccountId = A.DasAccountId
         LEFT JOIN
-     (
-         SELECT [DASAccountID]
-              , [LegalEntityName]
-			  , [PayeSchemeName]
-              , MAX([UpdateDateTime]) AS [Max_UpdateDateTime]
+		(
+         SELECT A2.[DasAccountID]
+              , LE2.[Name] AS LegalEntityName
+			  , Paye2.[Name] AS PayeSchemeName
+              , MAX([A2].[UpdateDateTime]) AS [Max_UpdateDateTime]
               , 1 AS [Flag_Latest]
          FROM
-            Data_Load.DAS_Employer_Registrations
-         GROUP BY [DASAccountID]
-                , [LegalEntityName]
-				, [PayeSchemeName]
-     ) AS B ON A.DASAccountID = B.DASAccountID
-               AND A.LegalEntityName = B.LegalEntityName
-			   AND A.PayeSchemeName = B.PayeSchemeName
+            Data_Load.DAS_Employer_Accounts A2
+			INNER JOIN Data_Load.DAS_Employer_LegalEntities AS LE2 ON LE2.DasAccountId = A2.DasAccountId
+			INNER JOIN Data_Load.DAS_Employer_PayeSchemes AS paye2 ON paye2.DasAccountId = A2.DasAccountId
+         GROUP BY A2.[DASAccountID]
+                , LE2.[Name]
+				, PAYE2.[Name]
+		) AS B ON A.DASAccountID = B.DASAccountID
+               AND LE.Name = B.LegalEntityName
+			   AND ISNULL(paye.Name, '') = ISNULL(B.PayeSchemeName, '')
                AND A.UpdateDateTime = B.Max_UpdateDateTime
