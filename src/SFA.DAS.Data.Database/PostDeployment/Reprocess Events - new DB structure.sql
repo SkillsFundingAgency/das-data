b6028aa605410f99c3af26e9e11a98aaa39cb4cd
Michael Young <mick.young1981@googlemail.com>
2017-02-03 08:33:32 +0000

    Added script to rerun events to populate the new table structure

---
+++ src/SFA.DAS.Data.Database/PostDeployment/Reprocess Events - new DB structure.sql
@@ -0,0 +1,5 @@
+﻿IF (SELECT COUNT(*) FROM Data_Load.DAS_Employer_Accounts) = 0
+BEGIN
+	DELETE FROM Data_Load.DAS_FailedEvents
+	UPDATE Data_Load.DAS_LoadedEvents SET LastProcessedEventId = 0 WHERE EventFeed = 'Accounts'
+END
