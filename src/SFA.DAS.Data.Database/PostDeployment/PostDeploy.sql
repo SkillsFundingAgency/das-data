f10a7a204a6b135080b41592183a6f057dc1ad17
Michael Young <mick.young1981@googlemail.com>
2017-02-10 08:04:18 +0000

    Removed data repopulation script.

--- src/SFA.DAS.Data.Database/PostDeployment/PostDeploy.sql
+++ src/SFA.DAS.Data.Database/PostDeployment/PostDeploy.sql
@@ -4,13 +4,6 @@ BEGIN
 	VALUES('AccountEvents', 0) 
 END 
 
-
-IF (SELECT COUNT(*) FROM Data_Load.DAS_Employer_Accounts) = 0
-BEGIN
-	DELETE FROM Data_Load.DAS_FailedEvents
-	UPDATE Data_Load.DAS_LoadedEvents SET LastProcessedEventId = 0 WHERE EventFeed = 'AccountEvents'
-END
-
 IF DATABASE_PRINCIPAL_ID('ViewSpecificReadOnly') IS NOT NULL
 BEGIN
 	GRANT SELECT ON [Data_Pub].[DAS_Employer_Accounts] TO ViewSpecificReadOnly
