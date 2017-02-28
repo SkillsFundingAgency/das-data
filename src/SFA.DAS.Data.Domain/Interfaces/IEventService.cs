df8a3bfaa484d397ab4082c374baeb9423979209
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-02-28 10:52:33 +0000

    Added events service to handle all events API requests

---
+++ src/SFA.DAS.Data.Domain/Interfaces/IEventService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.Data.Domain.Models;
+
+namespace SFA.DAS.Data.Domain.Interfaces
+{
+    public interface IEventService
+    {
+        ICollection<T> GetEvents<T>();
+
+        ICollection<CommitmentsApprenticeshipEvent> GetApprenticeshipEvents();
+    }
+}
---
+++ src/SFA.DAS.Data.Infrastructure/Services/EventsApiService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using SFA.DAS.Data.Domain.Interfaces;
+using SFA.DAS.Data.Domain.Models;
+using SFA.DAS.Events.Api.Client;
+
+namespace SFA.DAS.Data.Infrastructure.Services
+{
+    public class EventsApiService : IEventService
+    {
+        private readonly IEventsApi _eventsApi;
+
+        public EventsApiService(IEventsApi eventsApi)
+        {
+            _eventsApi = eventsApi;
+        }
+
+        public ICollection<T> GetEvents<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<CommitmentsApprenticeshipEvent> GetApprenticeshipEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
