53aa7dadb6d3c98fa9dc8e12f837bde1190c3f4a
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-03-01 15:17:55 +0000

    Finished off logging

--- src/SFA.DAS.Data.Worker/Events/EventsProcessor.cs
+++ src/SFA.DAS.Data.Worker/Events/EventsProcessor.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NLog;
 using SFA.DAS.Data.Worker.Events.EventHandlers;
 using SFA.DAS.Data.Worker.Events.EventsCollectors;
 
@@ -8,20 +10,29 @@ namespace SFA.DAS.Data.Worker.Events
     {
         private readonly IEventsCollector<T> _collector;
         private readonly IEventHandler<T> _handler;
+        private readonly ILogger _logger;
 
-        public EventsProcessor(IEventsCollector<T> collector, IEventHandler<T> handler)
+        public EventsProcessor(IEventsCollector<T> collector, IEventHandler<T> handler, ILogger logger)
         {
             _collector = collector;
             _handler = handler;
+            _logger = logger;
         }
 
         public async Task ProcessEvents()
         {
+            _logger.Info($"Processing {typeof(T).FullName} events");
+            
             var events = await _collector.GetEvents();
 
-            foreach (var @event in events)
+            if (events != null && events.Any())
             {
-                _handler.Handle(@event);
+                _logger.Info($"Collected {events.Count} events");
+
+                foreach (var @event in events)
+                {
+                    _handler.Handle(@event);
+                }
             }
         }
     }
