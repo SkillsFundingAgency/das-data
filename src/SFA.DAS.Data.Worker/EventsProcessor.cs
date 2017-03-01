95612bd0947651a6b991d519ef8efcba85805483
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-03-01 09:22:32 +0000

    Added Events Processors
    The events processors connect the event collectors to the event handlers

---
+++ src/SFA.DAS.Data.Worker/EventsProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.Data.Worker.EventHandlers;
+using SFA.DAS.Data.Worker.EventsCollectors;
+
+namespace SFA.DAS.Data.Worker
+{
+    public class EventsProcessor<T> : IEventsProcessor
+    {
+        private readonly IEventsCollector<T> _collector;
+        private readonly IEventHandler<T> _handler;
+
+        public EventsProcessor(IEventsCollector<T> collector, IEventHandler<T> handler)
+        {
+            _collector = collector;
+            _handler = handler;
+        }
+
+        public async Task ProcessEvents()
+        {
+            var events = await _collector.GetEvents();
+
+            foreach (var @event in events)
+            {
+                _handler.Handle(@event);
+            }
+        }
+    }
+}
