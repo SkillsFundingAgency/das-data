20d5e606fec308c9a33bafa11809c1a41b368d18
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-03-01 13:28:58 +0000

    Added logic and tests for event factory

--- src/SFA.DAS.Data.Worker/Factories/JsonEventModelFactory.cs
+++ src/SFA.DAS.Data.Worker/Factories/JsonEventModelFactory.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using Newtonsoft.Json;
+using NLog;
 
 namespace SFA.DAS.Data.Worker.Factories
 {
     public class JsonEventModelFactory : IEventModelFactory
     {
+        private readonly ILogger _logger;
+
+        public JsonEventModelFactory(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public T Create<T>(string data)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(data);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Could not create model of type");
+                return default(T);
+            }
         }
     }
 }
