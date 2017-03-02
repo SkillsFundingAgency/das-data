5951b58acf91c9d4ccb4ac555cb2356cfea5dfb9
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-03-02 14:16:45 +0000

    Create abstract handler to hold common event handling code

---
+++ src/SFA.DAS.Data.Worker/Events/EventHandlers/EventHandler.cs
@@ -0,0 +1,23 @@
+﻿using SFA.DAS.Data.Application.Interfaces.Repositories;
+using SFA.DAS.Events.Api.Types;
+
+namespace SFA.DAS.Data.Worker.Events.EventHandlers
+{
+    public abstract class EventHandler<T> : IEventHandler<T> where T : IEventView
+    {
+        protected readonly IEventRepository EventRepository;
+
+        protected EventHandler(IEventRepository eventRepository)
+        {
+            EventRepository = eventRepository;
+        }
+
+        public void Handle(T @event)
+        {
+            ProcessEvent(@event);
+            EventRepository.StoreLastProcessedEventId(nameof(T), @event.Id);
+        }
+
+        protected abstract void ProcessEvent(T @event);
+    }
+}
--- src/SFA.DAS.Data.Worker/Events/EventHandlers/IEventHandler.cs
+++ src/SFA.DAS.Data.Worker/Events/EventHandlers/IEventHandler.cs
@@ -1,6 +1,9 @@
-﻿namespace SFA.DAS.Data.Worker.Events.EventHandlers
+﻿using SFA.DAS.Data.Domain.Interfaces;
+using SFA.DAS.Events.Api.Types;
+
+namespace SFA.DAS.Data.Worker.Events.EventHandlers
 {
-    public interface IEventHandler<in T>
+    public interface IEventHandler<in T> where T : IEventView
     {
         void Handle(T @event);
     }
