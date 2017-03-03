ee9a278afea63fc8b8dbf314bd59acb330f8f579
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-03-03 10:33:04 +0000

    Created interface for data configuration so it can be mocked

---
+++ src/SFA.DAS.Data.Application/Configuration/IDataConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using SFA.DAS.EAS.Account.Api.Client;
+
+namespace SFA.DAS.Data.Application.Configuration
+{
+    public interface IDataConfiguration
+    {
+        AccountApiConfiguration AccountsApi { get; }
+        string DatabaseConnectionString { get; }
+        EventsApiClientConfiguration EventsApi { get; }
+        int FailureTolerance { get; }
+    }
+}
