833c3cd4f8cc14bbdfe1e1a209319e1fe42c577c
Michael Young <mick.young1981@googlemail.com>
2017-04-04 10:40:28 +0100

    Added persistence of legal entity details for apprenticeships.

--- src/SFA.DAS.Data.Domain/Models/ApprenticeshipEvent.cs
+++ src/SFA.DAS.Data.Domain/Models/ApprenticeshipEvent.cs
@@ -32,5 +32,11 @@ namespace SFA.DAS.Data.Domain.Models
         public DateTime TrainingEndDate { get; set; }
 
         public decimal TrainingTotalCost { get; set; }
+
+        public string LegalEntityCode { get; set; }
+
+        public string LegalEntityName { get; set; }
+
+        public string LegalEntityOrganisationType { get; set; }
     }
 }
