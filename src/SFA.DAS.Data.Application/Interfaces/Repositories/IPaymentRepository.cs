335f770fc841c11edbfabbe97778941583616e72
Michael Young <mick.young1981@googlemail.com>
2017-04-04 14:52:26 +0100

    Added payments repository.

---
+++ src/SFA.DAS.Data.Application/Interfaces/Repositories/IPaymentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.Payments.Events.Api.Types;
+
+namespace SFA.DAS.Data.Application.Interfaces.Repositories
+{
+    public interface IPaymentRepository
+    {
+        Task SavePayment(Payment payment);
+    }
+}
---
+++ src/SFA.DAS.Data.Infrastructure/Data/PaymentRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using SFA.DAS.Data.Application.Interfaces.Repositories;
+using SFA.DAS.Payments.Events.Api.Types;
+
+namespace SFA.DAS.Data.Infrastructure.Data
+{
+    public class PaymentRepository : BaseRepository, IPaymentRepository
+    {
+        public PaymentRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task SavePayment(Payment payment)
+        {
+            await WithConnection(async c =>
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@PaymentId", payment.Id, DbType.Int64);
+                parameters.Add("@UkPrn", payment.Ukprn, DbType.Int64);
+                parameters.Add("@Uln", payment.Uln, DbType.Int64);
+                parameters.Add("@EmployerAccountId", payment.EmployerAccountId, DbType.String);
+                parameters.Add("@ApprenticeshipId", payment.ApprenticeshipId, DbType.Int64);
+                parameters.Add("@DeliveryMonth", payment.DeliveryPeriod.Month, DbType.Int32);
+                parameters.Add("@DeliveryYear", payment.DeliveryPeriod.Year, DbType.Int32);
+                parameters.Add("@CollectionMonth", payment.CollectionPeriod.Month, DbType.Int32);
+                parameters.Add("@CollectionYear", payment.CollectionPeriod.Year, DbType.Int32);
+                parameters.Add("@EvidenceSubmittedOn", payment.EvidenceSubmittedOn, DbType.DateTime);
+                parameters.Add("@EmployerAccountVersion", payment.EmployerAccountVersion, DbType.String);
+                parameters.Add("@ApprenticeshipVersion", payment.ApprenticeshipVersion, DbType.String);
+                parameters.Add("@FundingSource", payment.FundingSource.ToString(), DbType.String);
+                parameters.Add("@TransactionType", payment.TransactionType.ToString(), DbType.String);
+                parameters.Add("@Amount", payment.Amount, DbType.Decimal);
+                parameters.Add("@StandardCode", payment.StandardCode, DbType.Int64);
+                parameters.Add("@FrameworkCode", payment.FrameworkCode, DbType.Int32);
+                parameters.Add("@ProgrammeType", payment.ProgrammeType, DbType.Int32);
+                parameters.Add("@PathwayCode", payment.PathwayCode, DbType.Int32);
+                //TODO: Remove this once the nuget package has been updated.
+                //parameters.Add("@ContractType", payment.ContractType.ToString(), DbType.String);
+
+                return await c.ExecuteAsync(
+                    sql: "[Data_Load].[SavePayment]",
+                    param: parameters,
+                    commandType: CommandType.StoredProcedure);
+            });
+        }
+    }
+}
