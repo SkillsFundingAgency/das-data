aba15bf2fd07203c639f879a0ea0b1a84852d773
Stuart Baillie <stuart.baillie@fasst.org.uk>
2017-04-06 12:16:50 +0100

    Fixed Flag_latest and add Legal EntityID

--- src/SFA.DAS.Data.Database/Views/DAS_Commitments.sql
+++ src/SFA.DAS.Data.Database/Views/DAS_Commitments.sql
@@ -1,5 +1,7 @@
 CREATE VIEW Data_Pub.DAS_Commitments
 AS
+     CREATE VIEW Data_Pub.DAS_Commitments
+AS
      SELECT [C].[ID]
           , [C].[CommitmentID]
           , [C].[PaymentStatus]
@@ -41,19 +43,31 @@ AS
           , CAST([C].[UpdateDateTime] AS DATE) AS [UpdateDate]
             -- Flag to say if latest record from subquery, Using Coalesce to set null value to 0
           , COALESCE([LC].[Flag_Latest], 0) AS [Flag_Latest]
+		, C.[LegalEntityCode]
+		, C.[LegalEntityName]
+		, C.[LegalEntityOrganisationType] AS LegalEntitySource
+		, COALESCE(ELE.[DasLegalEntityId],-1) AS [DasLegalEntityId]
      FROM
         Data_Load.DAS_Commitments AS C
-    -- To get las
+    -- To get latest record
         LEFT JOIN
      (
-         SELECT [C].[CommitmentId]
+         SELECT [C].[ApprenticeshipId]
               , MAX([C].[UpdateDateTime]) AS [Max_UpdatedDateTime]
-              , 1 AS [Flag_Latest]
+              , MAX([C].ID)  AS Max_ID
+		    , 1 AS [Flag_Latest]
          FROM
             Data_Load.DAS_Commitments AS C
-         GROUP BY [C].[CommitmentId]
-     ) AS LC ON C.CommitmentId = LC.CommitmentId
-                AND LC.Max_UpdatedDateTime = C.UpdateDateTime
+         GROUP BY [C].[ApprenticeshipId]
+     ) AS LC ON LC.ApprenticeshipId = C.ApprenticeshipId
+                AND LC.Max_ID = C.ID
+			 AND LC.Max_UpdatedDateTime = C.UpdateDateTime
 	-- Join to Accounts to get the Hashed DAS Acccount ID
-	LEFT JOIN  [Data_Load].[DAS_Employer_Accounts] AS EA ON EA.AccountID = [C].[EmployerAccountID];
+	LEFT JOIN  [Data_Load].[DAS_Employer_Accounts] AS EA ON EA.AccountID = [C].[EmployerAccountID]
+	---- Join Legal Entity to get Legal_Entity_ID
+	LEFT JOIN Data_Pub.DAS_Employer_LegalEntities AS ELE ON C.LegalEntityOrganisationType = ELE.[LegalEntitySource]
+												AND CASE WHEN C.LegalEntityOrganisationType IN ('PublicBodies','Other') THEN '' ELSE C.[LegalEntityCode] END = ELE.[LegalEntityNumber]
+												AND C.[LegalEntityName] = ELE.LegalEntityName 
+												AND ELE.Flag_latest = 1
+	;
 GO
