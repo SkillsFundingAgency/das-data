74af9898571c5d5f0e1107fdc28b7b6cb83a5304
Michael Young <mick.young1981@googlemail.com>
2017-05-17 15:09:10 +0100

    Added calendar months.

---
+++ src/SFA.DAS.Data.Database/Tables/DAS_CalendarMonth.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Data_Load.DAS_CalendarMonth
+(
+    CalendarMonthID INT NOT NULL PRIMARY KEY
+    , CalendarDate DATE NOT NULL
+    , CalendarMonthNumber INT NOT NULL
+    , CalendarMonthName VARCHAR(20) NOT NULL
+    , CalendarMonthShortName VARCHAR(20) NOT NULL
+    , CalendarYear INT NOT NULL
+    , CalendarMonthNameYear VARCHAR(20) NOT NULL
+    , CalendarMonthShortNameYear VARCHAR(20) NOT NULL
+    , TaxMonthNumber VARCHAR(20) NOT NULL
+    , TaxYear VARCHAR(20) NOT NULL
+    , AcademicMonthNumber INT NOT NULL
+    , AcademicYear VARCHAR(20) NOT NULL
+)
---
+++ src/SFA.DAS.Data.Database/Views/DAS_CalendarMonth.sql
@@ -0,0 +1,29 @@
+﻿CREATE VIEW Data_Pub.DAS_CalendarMonth
+
+AS
+
+SELECT [CalendarMonthID]
+
+      ,[CalendarDate]
+
+      ,[CalendarMonthNumber]
+
+      ,[CalendarMonthName]
+
+      ,[CalendarMonthShortName]
+
+      ,[CalendarYear]
+
+      ,[CalendarMonthNameYear]
+
+      ,[CalendarMonthShortNameYear]
+
+      ,[TaxMonthNumber]
+
+      ,[TaxYear]
+
+      ,[AcademicMonthNumber]
+
+      ,[AcademicYear]
+
+  FROM [Data_Load].[DAS_CalendarMonth]
