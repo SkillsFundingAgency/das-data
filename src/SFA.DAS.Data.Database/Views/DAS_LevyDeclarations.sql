484a97ed9e9506d0993a506395e84873cde45608
Jake Benilov <benilov@gmail.com>
2017-05-17 18:20:57 +0100

    Make indentation in LevyDeclarations view more consistent
    There are no differences with 'git diff -w'.

--- src/SFA.DAS.Data.Database/Views/DAS_LevyDeclarations.sql
+++ src/SFA.DAS.Data.Database/Views/DAS_LevyDeclarations.sql
@@ -3,17 +3,17 @@ AS
 
 SELECT LD.[Id]
       ,LD.[DasAccountId] AS DASAccountID
-      ,LD.[LevyDeclarationId] AS LevyDeclarationID 
+      ,LD.[LevyDeclarationId] AS LevyDeclarationID
       ,HASHBYTES('SHA2_512',RTRIM(LTRIM(CAST(LD.[PayeSchemeReference] AS VARCHAR(20))))) AS PAYEReference
       ,LD.[LevyDueYearToDate]
       ,LD.[LevyAllowanceForYear]
       ,LD.[SubmissionDate] AS SubmissionDateTime
-	 ,CAST(LD.[SubmissionDate] AS DATE) AS SubmissionDate
+      ,CAST(LD.[SubmissionDate] AS DATE) AS SubmissionDate
       ,LD.[SubmissionId] AS SubmissionID
       ,LD.[PayrollYear]
       ,LD.[PayrollMonth]
       ,LD.[CreatedDate] AS CreatedDateTime
-	 ,CAST(LD.CreatedDate AS DATE) AS CreatedDate
+      ,CAST(LD.CreatedDate AS DATE) AS CreatedDate
       ,LD.[EndOfYearAdjustment]
       ,LD.[EndOfYearAdjustmentAmount]
       ,LD.[DateCeased]
@@ -24,31 +24,31 @@ SELECT LD.[Id]
       ,LD.[TopupPercentage]
       ,LD.[TopupAmount]
       ,LD.[UpdateDateTime]
-	 -- Additional Columns for UpdateDateTime represented as a Date
-	,	CAST(LD.[UpdateDateTime] AS DATE) AS UpdateDate
-	-- Flag to say if latest record from subquery, Using Coalesce to set null value to 0
+      -- Additional Columns for UpdateDateTime represented as a Date
+      ,CAST(LD.[UpdateDateTime] AS DATE) AS UpdateDate
+      -- Flag to say if latest record from subquery, Using Coalesce to set null value to 0
 
-	, COALESCE(LLD.Flag_Latest,0) AS Flag_Latest 
+  , COALESCE(LLD.Flag_Latest,0) AS Flag_Latest
   FROM [Data_Load].[DAS_LevyDeclarations] AS LD
  -- -- Adding flag to say latest record for Account ID
-	LEFT JOIN (
-				SELECT		
-					   LD.DasAccountId
-				    ,   LD.PayeSchemeReference
-				    ,   LD.PayrollYear
-				    ,   LD.PayrollMonth
-				    ,   MAX(SubmissionID) AS Max_SubmissionID
-				    ,   1 AS Flag_Latest 
-			FROM Data_Load.DAS_LevyDeclarations AS LD
-			GROUP BY 
-					   LD.DasAccountId
-				    ,   LD.PayeSchemeReference
-				    ,   LD.PayrollYear
-				    ,   LD.PayrollMonth
-			) AS LLD ON LLD.DasAccountId = LD.DasAccountId
-					AND LLD.PayeSchemeReference = LD.PayeSchemeReference
-					AND LLD.PayrollYear = LD.PayrollYear
-					AND LLD.PayrollMonth = LD.PayrollMonth
-					AND LLD.Max_SubmissionID = LD.SubmissionID;
+  LEFT JOIN (
+        SELECT
+             LD.DasAccountId
+            ,   LD.PayeSchemeReference
+            ,   LD.PayrollYear
+            ,   LD.PayrollMonth
+            ,   MAX(SubmissionID) AS Max_SubmissionID
+            ,   1 AS Flag_Latest
+      FROM Data_Load.DAS_LevyDeclarations AS LD
+      GROUP BY
+             LD.DasAccountId
+            ,   LD.PayeSchemeReference
+            ,   LD.PayrollYear
+            ,   LD.PayrollMonth
+      ) AS LLD ON LLD.DasAccountId = LD.DasAccountId
+          AND LLD.PayeSchemeReference = LD.PayeSchemeReference
+          AND LLD.PayrollYear = LD.PayrollYear
+          AND LLD.PayrollMonth = LD.PayrollMonth
+          AND LLD.Max_SubmissionID = LD.SubmissionID;
 
 GO
