724537a16e7b3562a0e97434846f717ddc6fda4b
Stuart Baillie <stuart.baillie@fasst.org.uk>
2017-05-17 16:50:37 +0100

    Update DAS_LevyDeclarations.sql
    Added PayrollMonthShortNameYear, LevyDeclaredInMonth and LevyAvailableInMonth

--- src/SFA.DAS.Data.Database/Views/DAS_LevyDeclarations.sql
+++ src/SFA.DAS.Data.Database/Views/DAS_LevyDeclarations.sql
@@ -25,10 +25,13 @@ SELECT LD.[Id]
       ,LD.[TopupAmount]
       ,LD.[UpdateDateTime]
 	 -- Additional Columns for UpdateDateTime represented as a Date
-	,	CAST(LD.[UpdateDateTime] AS DATE) AS UpdateDate
+      ,CAST(LD.[UpdateDateTime] AS DATE) AS UpdateDate
 	-- Flag to say if latest record from subquery, Using Coalesce to set null value to 0
+      ,COALESCE(LLD.Flag_Latest,0) AS Flag_Latest 
+      ,CM.CalendarMonthShortNameYear AS PayrollMonthShortNameYear
+      ,LD.LevyDeclaredInMonth
+      ,LD.LevyAvailableInMonth
 
-	, COALESCE(LLD.Flag_Latest,0) AS Flag_Latest 
   FROM [Data_Load].[DAS_LevyDeclarations] AS LD
  -- -- Adding flag to say latest record for Account ID
 	LEFT JOIN (
@@ -49,6 +52,8 @@ SELECT LD.[Id]
 					AND LLD.PayeSchemeReference = LD.PayeSchemeReference
 					AND LLD.PayrollYear = LD.PayrollYear
 					AND LLD.PayrollMonth = LD.PayrollMonth
-					AND LLD.Max_SubmissionID = LD.SubmissionID;
+					AND LLD.Max_SubmissionID = LD.SubmissionID
+	 LEFT JOIN Data_Load.DAS_CalendarMonth AS CM ON LD.PayrollYear = CM.TaxYear
+							AND LD.PayrollMonth = CM.TaxMonthNumber;
 
 GO
