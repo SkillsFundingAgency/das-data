7fd2514d40eb11bd87e5da389b9eff48ae3fc91e
Stuart Baillie <stuart.baillie@fasst.org.uk>
2017-05-18 08:19:28 +0100

    Update DAS_LevyDeclarations.sql
    tidied up formatting a bit

--- src/SFA.DAS.Data.Database/Views/DAS_LevyDeclarations.sql
+++ src/SFA.DAS.Data.Database/Views/DAS_LevyDeclarations.sql
@@ -8,12 +8,12 @@ SELECT LD.[Id]
       ,LD.[LevyDueYearToDate]
       ,LD.[LevyAllowanceForYear]
       ,LD.[SubmissionDate] AS SubmissionDateTime
-	 ,CAST(LD.[SubmissionDate] AS DATE) AS SubmissionDate
+      ,CAST(LD.[SubmissionDate] AS DATE) AS SubmissionDate
       ,LD.[SubmissionId] AS SubmissionID
       ,LD.[PayrollYear]
       ,LD.[PayrollMonth]
       ,LD.[CreatedDate] AS CreatedDateTime
-	 ,CAST(LD.CreatedDate AS DATE) AS CreatedDate
+      ,CAST(LD.CreatedDate AS DATE) AS CreatedDate
       ,LD.[EndOfYearAdjustment]
       ,LD.[EndOfYearAdjustmentAmount]
       ,LD.[DateCeased]
@@ -25,14 +25,12 @@ SELECT LD.[Id]
       ,LD.[TopupAmount]
       ,LD.[UpdateDateTime]
 	 -- Additional Columns for UpdateDateTime represented as a Date
-	,	CAST(LD.[UpdateDateTime] AS DATE) AS UpdateDate
+      ,	CAST(LD.[UpdateDateTime] AS DATE) AS UpdateDate
 	-- Flag to say if latest record from subquery, Using Coalesce to set null value to 0
-
-	, COALESCE(LLD.Flag_Latest,0) AS Flag_Latest 
-	, CM.CalendarMonthShortNameYear AS PayrollMonthShortNameYear
-	, LD.LevyDeclaredInMonth
-    , LD.LevyAvailableInMonth
-
+     , COALESCE(LLD.Flag_Latest,0) AS Flag_Latest 
+     , CM.CalendarMonthShortNameYear AS PayrollMonthShortNameYear
+     , LD.LevyDeclaredInMonth
+     , LD.LevyAvailableInMonth
   FROM [Data_Load].[DAS_LevyDeclarations] AS LD
  -- -- Adding flag to say latest record for Account ID after getting highest submission date using Update data time to get back one row
 	LEFT JOIN (
@@ -58,12 +56,12 @@ SELECT LD.[Id]
 				                        ,   LD.PayeSchemeReference
 				                        ,   LD.PayrollYear
 				                        ,   LD.PayrollMonth
-			     ) AS LLD ON LLD.DasAccountId = LD.DasAccountId
+			     				) AS LLD ON LLD.DasAccountId = LD.DasAccountId
 					     AND LLD.PayeSchemeReference = LD.PayeSchemeReference
 					     AND LLD.PayrollYear = LD.PayrollYear
 					     AND LLD.PayrollMonth = LD.PayrollMonth
 					     AND LLD.Max_SubmissionID = LD.SubmissionID
-                    GROUP BY 
+                    			GROUP BY 
 					          LD.DasAccountId
 				          ,   LD.PayeSchemeReference
 				          ,   LD.PayrollYear
@@ -74,7 +72,7 @@ SELECT LD.[Id]
 					AND LLD.PayrollYear = LD.PayrollYear
 					AND LLD.PayrollMonth = LD.PayrollMonth
 					AND LLD.SubmissionID = LD.SubmissionID
-                         AND LLD.Max_UpdateDateTime = LD.UpdateDateTime
+                         		AND LLD.Max_UpdateDateTime = LD.UpdateDateTime
                
 	 LEFT JOIN Data_Load.DAS_CalendarMonth AS CM ON LD.PayrollYear = CM.TaxYear
 										  AND LD.PayrollMonth = CM.TaxMonthNumber;
