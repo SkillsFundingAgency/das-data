f0952cf446cc02f9a3524ef00748c0bc64afc7ed
Michael Young <mick.young1981@googlemail.com>
2017-05-24 09:31:42 +0100

    Added roatp/provider database tables.

---
+++ src/SFA.DAS.Data.Database/Tables/DAS_Providers.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [Data_Load].[DAS_Providers]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	[UkPrn] BIGINT NOT NULL,
+	[IsHigherEducationInstitute] BIT NOT NULL,
+	[ProviderName] NVARCHAR(100) NOT NULL,
+	[IsEmployerProvider] BIT NOT NULL,
+	[Phone] NVARCHAR(255),
+	[Email] NVARCHAR(255),
+	[NationalProvider] BIT NOT NULL,
+	[Website] NVARCHAR(255),
+	[EmployerSatisfaction] DECIMAL(18,5) NOT NULL,
+	[LearnerSatisfaction] DECIMAL(18,5) NOT NULL,
+	[UpdateDateTime] DATETIME NOT NULL DEFAULT (GETDATE())
+)
---
+++ src/SFA.DAS.Data.Database/Tables/DAS_Roatp.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Data_Load].[DAS_Roatp]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	[UkPrn] BIGINT NOT NULL,
+	[ProviderType] NVARCHAR(50) NOT NULL,
+	[ParentCompanyGuarantee] BIT NOT NULL,
+	[NewOrganisationWithoutFinancialTrackRecord] BIT NOT NULL,
+	[StartDate] DATETIME NOT NULL,
+	[UpdateDateTime] DATETIME NOT NULL DEFAULT (GETDATE())
+)
