0fa8e9fee74334eb1f5184b8f49e1fa17f9daa84
Michael Young <mick.young1981@googlemail.com>
2017-05-24 10:06:35 +0100

    Added roatp repository

---
+++ src/SFA.DAS.Data.Application/Interfaces/Repositories/IRoatpRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.Data.Application.Interfaces.Repositories
+{
+    public interface IRoatpRepository
+    {
+        Task SaveRoatpProvider(Roatp.Api.Types.Provider provider);
+    }
+}
---
+++ src/SFA.DAS.Data.Infrastructure/Data/RoatpRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using SFA.DAS.Data.Application.Interfaces.Repositories;
+
+namespace SFA.DAS.Data.Infrastructure.Data
+{
+    public class RoatpRepository : BaseRepository, IRoatpRepository
+    {
+        public RoatpRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task SaveRoatpProvider(Roatp.Api.Types.Provider provider)
+        {
+            await WithConnection(async c =>
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@UkPrn", provider.Ukprn, DbType.Int64);
+                parameters.Add("@ProviderType", provider.ProviderType.ToString(), DbType.String);
+                parameters.Add("@ParentCompanyGuarantee", provider.ParentCompanyGuarantee, DbType.Boolean);
+                parameters.Add("@NewOrganisationWithoutFinancialTrackRecord", provider.NewOrganisationWithoutFinancialTrackRecord, DbType.Boolean);
+                parameters.Add("@StartDate", provider.StartDate, DbType.DateTime);
+
+                return await c.ExecuteAsync(
+                    sql: "[Data_Load].[SaveRoatp]",
+                    param: parameters,
+                    commandType: CommandType.StoredProcedure);
+            });
+        }
+    }
+}
