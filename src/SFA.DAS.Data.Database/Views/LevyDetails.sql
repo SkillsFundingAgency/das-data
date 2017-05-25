6949f43943b33184027debdb3bf6dd6df186d298
Stuart Baillie <stuart.baillie@fasst.org.uk>
2017-05-25 09:01:37 +0100

    Update LevyDetails.sql
    Cosmetic   refactor plus adding PayrollMonthShortNameYear, Levy.LevyDeclaredInMonth and  Levy.LevyAvailableInMonth Columns

--- src/SFA.DAS.Data.Database/Views/LevyDetails.sql
+++ src/SFA.DAS.Data.Database/Views/LevyDetails.sql
@@ -1,30 +1,35 @@
-﻿CREATE VIEW Reporting.LevyDetails AS
-SELECT
-    LEVY.[DasAccountId],
-    [DASAccountName],
-    [DateRegistered],
-    [LevyDeclarationID],
-    Levy.[PAYEReference]
-    ,[LevyDueYearToDate]
-    ,[LevyAllowanceForYear]
-    ,[SubmissionDate]
-    ,[SubmissionID]
-    ,[PayrollYear]
-    ,[PayrollMonth]
-    ,[CreatedDate]
-    ,[EndOfYearAdjustment]
-    ,[EndOfYearAdjustmentAmount]
-    ,[DateCeased]
-    ,[InactiveFrom]
-    ,[InactiveTo]
-    ,[HMRCSubmissionID]
-    ,[EnglishFraction]
-    ,[TopupPercentage]
-    ,[TopupAmount]
-       ,Levy.updatedatetime
-FROM [Data_Pub].[DAS_LevyDeclarations] Levy
-       INNER JOIN [Data_pub].[DAS_Employer_PayeSchemes] PAYE
-       ON PAYE.[PAYEReference] = Levy.[PAYEReference] AND PAYE.DasAccountID = Levy.DASAccountID
-       INNER JOIN [Data_Pub].[DAS_Employer_Accounts] Accounts
-       ON PAYE.[DASAccountID]= Accounts.[DASAccountID]
-WHERE Levy.flag_latest=1 and PAYE.Flag_Latest=1 and Accounts.flag_latest=1
+CREATE VIEW Reporting.LevyDetails AS
+SELECT 
+	   Levy.DasAccountId
+     , Accounts.DASAccountName
+     , Accounts.DateRegistered
+     , Levy.LevyDeclarationID
+     , Levy.PAYEReference
+     , Levy.LevyDueYearToDate
+     , Levy.LevyAllowanceForYear
+     , Levy.SubmissionDate
+     , Levy.SubmissionID
+     , Levy.PayrollYear
+     , Levy.PayrollMonth
+     , Levy.CreatedDate
+     , Levy.EndOfYearAdjustment
+     , Levy.EndOfYearAdjustmentAmount
+     , Levy.DateCeased
+     , Levy.InactiveFrom
+     , Levy.InactiveTo
+     , Levy.HMRCSubmissionID
+     , Levy.EnglishFraction
+     , Levy.TopupPercentage
+     , Levy.TopupAmount
+     , Levy.updatedatetime
+	 , Levy.PayrollMonthShortNameYear
+	 , Levy.LevyDeclaredInMonth
+	 , Levy.LevyAvailableInMonth
+FROM
+   Data_Pub.DAS_LevyDeclarations Levy
+   INNER JOIN Data_pub.DAS_Employer_PayeSchemes PAYE ON PAYE.PAYEReference = Levy.PAYEReference
+                                                        AND PAYE.DasAccountID = Levy.DASAccountID
+   INNER JOIN Data_Pub.DAS_Employer_Accounts Accounts ON PAYE.DASAccountID = Accounts.DASAccountID
+WHERE Levy.flag_latest = 1
+      AND PAYE.Flag_Latest = 1
+      AND Accounts.flag_latest = 1;
