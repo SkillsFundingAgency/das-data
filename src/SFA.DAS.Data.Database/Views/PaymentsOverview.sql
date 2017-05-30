3aab42a20e557e78c7bcf074e50551d38656fcd8
Stuart Baillie <stuart.baillie@fasst.org.uk>
2017-05-30 11:27:25 +0100

    Update PaymentsOverview.sql
    Fix for Mayura

--- src/SFA.DAS.Data.Database/Views/PaymentsOverview.sql
+++ src/SFA.DAS.Data.Database/Views/PaymentsOverview.sql
@@ -1,42 +1,38 @@
-﻿CREATE VIEW [Reporting].[PaymentsOverview]
-AS
-SELECT
-CASE
-       WHEN (C.[AgreementStatus] like'BothAgreed') THEN 'Agreed commitments to date'
-       ELSE 'Non Agreed commitments to date'
-END AS CommitmentStatus
-,COUNT ((C.[CommitmentID])) AS Commitments
-,Payment = SUM(Amount)
- 
-FROM [Data_pub].[Das_Commitments] AS C
-               Join [Data_Pub].[DAS_Payments] AS P
-                     On C.[DASAccountID] = P.[DASAccountID]
-                           AND C.CommitmentID = P.CommitmentID
-WHERE  C.Flag_Latest = 1
-GROUP BY
-     CASE
-       WHEN (C.[AgreementStatus] like'BothAgreed') THEN 'Agreed commitments to date'
-       ELSE 'Non Agreed commitments to date'
-END
- 
- 
-UNION
- 
-SELECT
-CASE
-       WHEN (C.[AgreementStatus]like'BothAgreed') THEN 'Agreed commitments with start date this month'
-       ELSE 'Non agreed commitments to date with start date this month'
-END AS CommitmentStatus
-, COUNT ((C.[CommitmentID])) AS Commitments
-, Payment =Sum(Amount)
- 
-FROM [Data_pub].[Das_Commitments] AS C
-               Join [Data_Pub].[DAS_Payments] AS P
-                     On C.[DASAccountID] = P.[DASAccountID]
-                           AND C.CommitmentID = P.CommitmentID
-WHERE C.StartDateInCurrentMonth = 'Yes' AND C.Flag_Latest = 1
-GROUP BY
+CREATE VIEW [Reporting].[PaymentsOverview]
+       AS
+       SELECT
        CASE
-       WHEN (C.[AgreementStatus]like'BothAgreed') THEN 'Agreed commitments with start date this month'
-       ELSE 'Non agreed commitments to date with start date this month'
-END
+              WHEN (C.[AgreementStatus] like'BothAgreed') THEN 'Agreed commitments to date'
+              ELSE 'Non Agreed commitments to date'
+       END AS CommitmentStatus
+       ,COUNT ((C.[CommitmentID])) AS Commitments
+       
+       
+       FROM [Data_pub].[Das_Commitments] AS C
+                    
+       WHERE  C.Flag_Latest = 1
+       GROUP BY
+            CASE
+              WHEN (C.[AgreementStatus] like'BothAgreed') THEN 'Agreed commitments to date'
+              ELSE 'Non Agreed commitments to date'
+       END
+       
+        
+       UNION
+       
+       SELECT
+       CASE
+              WHEN (C.[AgreementStatus]like'BothAgreed') THEN 'Agreed commitments with start date this month'
+              ELSE 'Non agreed commitments with start date this month'
+       END AS CommitmentStatus
+       , COUNT ((C.[CommitmentID])) AS Commitments
+       
+       
+       FROM [Data_pub].[Das_Commitments] AS C
+                    
+       WHERE C.StartDateInCurrentMonth = 'Yes' AND C.Flag_Latest = 1
+       GROUP BY
+              CASE
+              WHEN (C.[AgreementStatus]like'BothAgreed') THEN 'Agreed commitments with start date this month'
+              ELSE 'Non agreed commitments with start date this month'
+       END
