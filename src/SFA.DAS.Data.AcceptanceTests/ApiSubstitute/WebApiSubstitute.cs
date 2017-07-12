15197fc46c382c53887f2086bee4b228539b9327
Michael Young <mick.young1981@googlemail.com>
2017-07-12 09:44:31 +0100

    Test performance improvements.

--- src/SFA.DAS.Data.AcceptanceTests/ApiSubstitute/WebApiSubstitute.cs
+++ src/SFA.DAS.Data.AcceptanceTests/ApiSubstitute/WebApiSubstitute.cs
@@ -6,27 +6,34 @@ namespace SFA.DAS.Data.AcceptanceTests.ApiSubstitute
 {
     public class WebApiSubstitute : IDisposable
     {
-        private readonly string _baseAddress;
+        private readonly string[] _baseAddresses;
         private ApiSubstituteMessageHandler _messageHandler;
         private IDisposable _webApp;
 
-        public WebApiSubstitute(string baseAddress)
+        public WebApiSubstitute(params string[] baseAddresses)
         {
-            _baseAddress = baseAddress;
+            _baseAddresses = baseAddresses;
             _messageHandler = new ApiSubstituteMessageHandler();
         }
 
         public void Start()
         {
-            var startOptions = new StartOptions(_baseAddress);
+            var startOptions = new StartOptions();
+            foreach (var address in _baseAddresses)
+            {
+                startOptions.Urls.Add(address);
+            }
             var apiStartup = new ApiStartup();
             _webApp = WebApp.Start(startOptions, builder => apiStartup.Configuration(builder, _messageHandler));
         }
 
         public void SetupGet(string apiPath, object returnValue)
         {
-            var fullUrl = _baseAddress + apiPath;
-            _messageHandler.SetupGet(fullUrl, returnValue);
+            foreach (var address in _baseAddresses)
+            {
+                var fullUrl = address + apiPath;
+                _messageHandler.SetupGet(fullUrl, returnValue);
+            }
         }
 
         public void ClearSetup()
