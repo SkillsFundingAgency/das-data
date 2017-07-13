ebd319c8db5341bb6e84ed166e69a8512ec40fac
Michael Young <mick.young1981@googlemail.com>
2017-07-13 08:41:54 +0100

    Publish legal entity statistics to the performance platform.

---
+++ src/SFA.DAS.Data.Application.UnitTests/DataExtractors/OrganisationsDataExtractorTests/WhenIExtractOrganisationData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Data.Application.DataExtractors;
+using SFA.DAS.Data.Application.Interfaces.Repositories;
+
+namespace SFA.DAS.Data.Application.UnitTests.DataExtractors.OrganisationsDataExtractorTests
+{
+    [TestFixture]
+    public class WhenIExtractOrganisationData
+    {
+        private const string ExpectedDataType = "organisations added";
+
+        private Mock<IPerformancePlatformRepository> _performancePlatformRepository;
+        private Mock<ILegalEntityRepository> _legalEntityRepository;
+        private OrganisationsDataExtractor _extractor;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _performancePlatformRepository = new Mock<IPerformancePlatformRepository>();
+            _legalEntityRepository = new Mock<ILegalEntityRepository>();
+
+            _extractor = new OrganisationsDataExtractor(_performancePlatformRepository.Object, _legalEntityRepository.Object);
+        }
+
+        [Test]
+        public async Task ThenTheNumberOfNewPayeSchemesSinceTheLastRunIsReturned()
+        {
+            var numberOfRecordsInLastRun = 45;
+            var currentNumberOfRecords = 50;
+            _performancePlatformRepository.Setup(x => x.GetNumberOfRecordsFromLastRun(ExpectedDataType)).ReturnsAsync(numberOfRecordsInLastRun);
+            _legalEntityRepository.Setup(x => x.GetTotalNumberOfLegalEntities()).ReturnsAsync(currentNumberOfRecords);
+
+            var extractDateTime = DateTime.Now;
+
+            var data = await _extractor.Extract(extractDateTime);
+
+            Assert.AreEqual(ExpectedDataType, data.Type);
+            Assert.AreEqual(extractDateTime.AddDays(-1).Date, data.Timestamp);
+            Assert.AreEqual(5, data.RecordsSinceLastRun);
+            Assert.AreEqual(currentNumberOfRecords, data.TotalNumberOfRecords);
+        }
+    }
+}
---
+++ src/SFA.DAS.Data.Application/DataExtractors/OrganisationsDataExtractor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.Data.Application.Interfaces;
+using SFA.DAS.Data.Application.Interfaces.Repositories;
+
+namespace SFA.DAS.Data.Application.DataExtractors
+{
+    public class OrganisationsDataExtractor : IPerformancePlatformDataExtractor
+    {
+        private const string DataType = "organisations added";
+
+        private IPerformancePlatformRepository _performancePlatformRepository;
+        private ILegalEntityRepository _legalEntityRepository;
+
+        public OrganisationsDataExtractor(IPerformancePlatformRepository performancePlatformRepository, ILegalEntityRepository legalEntityRepository)
+        {
+            _performancePlatformRepository = performancePlatformRepository;
+            _legalEntityRepository = legalEntityRepository;
+        }
+
+        public async Task<PerformancePlatformData> Extract(DateTime extractDateTime)
+        {
+            var currentRecordCount = await _legalEntityRepository.GetTotalNumberOfLegalEntities();
+            var newRecords = await GetNumberOfNewRecords(currentRecordCount);
+
+            return new PerformancePlatformData(extractDateTime.AddDays(-1).Date, DataType, newRecords, currentRecordCount);
+        }
+
+        private async Task<long> GetNumberOfNewRecords(long currentRecordCount)
+        {
+            var previousRecordCount = await _performancePlatformRepository.GetNumberOfRecordsFromLastRun(DataType);
+            
+            var newRecords = currentRecordCount - previousRecordCount;
+            return newRecords;
+        }
+    }
+}
