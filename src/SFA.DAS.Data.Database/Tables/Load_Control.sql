bc612041c88545f86d743be0bb4d862d4771a74c
Michael Young <mick.young1981@googlemail.com>
2017-12-14 08:20:39 +0000

    Added HMRC MI data load tables and procs

---
+++ src/SFA.DAS.Data.Database/Tables/Load_Control.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [HMRC].[Load_Control](
+     SourceFile_ID BIGINT IDENTITY(1,1)
+  ,  SourceFIle_Name VARCHAR(255)
+  ,  SourceFile_Status VARCHAR(50) 
+  ,  InsertDate DATETIME DEFAULT GETDATE()
+  ,  CreatedBy VARCHAR(255) DEFAULT System_user
+  ,  Flag_PastDataQaulityTests BIT DEFAULT 0
+  ,  Flag_LoadedSuccessfullyintoLiveTable BIT DEFAULT 0
+  ,  Flag_LoadedSuccessfullyintoHistoryTable BIT DEFAULT 0
+) 
---
+++ src/SFA.DAS.Data.Database/Tables/Process_Log.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [HMRC].[Process_Log](
+     ProcessEvent_ID BIGINT IDENTITY(1,1)
+  ,  ProcessDateTime DATETIME DEFAULT GETDATE()
+  ,  ProcessEventName VARCHAR(255)
+  ,  ProcessEventDescription VARCHAR(2000)
+  ,  ProcessEventCreatedBy VARCHAR(255) DEFAULT System_user
+  ,  SourceFile_ID BIGINT
+) 
