d29bd70150a851bc488e0b067c489e009c8e30be
Michael Young <mick.young1981@googlemail.com>
2017-12-29 08:12:14 +0000

    Added employment check repository

---
+++ src/SFA.DAS.Data.Application/Interfaces/Repositories/IEmploymentCheckRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmploymentCheck.Events;
+
+namespace SFA.DAS.Data.Application.Interfaces.Repositories
+{
+    public interface IEmploymentCheckRepository
+    {
+        Task SaveEmploymentCheck(EmploymentCheckCompleteEvent employmentCheck);
+    }
+}
