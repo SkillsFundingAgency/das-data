bf6f01506693cfbca66c740b4470f45f732b2d3b
Michael Young <mick.young1981@googlemail.com>
2017-12-29 08:20:55 +0000

    Added command to save employment check

---
+++ src/SFA.DAS.Data.Application/Commands/CreateEmploymentCheck/CreateEmploymentCheckCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Data.Application.Interfaces.Repositories;
+
+namespace SFA.DAS.Data.Application.Commands.CreateEmploymentCheck
+{
+    public class CreateEmploymentCheckCommandHandler : IAsyncNotificationHandler<CreateEmploymentCheckCommand>
+    {
+        private readonly IEmploymentCheckRepository _repository;
+
+        public CreateEmploymentCheckCommandHandler(IEmploymentCheckRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateEmploymentCheckCommand command)
+        {
+            await _repository.SaveEmploymentCheck(command.Event);
+        }
+    }
+}
