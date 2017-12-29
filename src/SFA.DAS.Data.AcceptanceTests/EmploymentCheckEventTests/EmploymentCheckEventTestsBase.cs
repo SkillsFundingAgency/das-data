33cdb4883b50cf87c415955f91a32e3e3c962e14
Michael Young <mick.young1981@googlemail.com>
2017-12-29 08:43:13 +0000

    Added acceptance test

---
+++ src/SFA.DAS.Data.AcceptanceTests/EmploymentCheckEventTests/EmploymentCheckEventTestsBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.Data.AcceptanceTests.ApiSubstitute;
+using SFA.DAS.Data.AcceptanceTests.Data;
+using SFA.DAS.Data.Worker;
+
+namespace SFA.DAS.Data.AcceptanceTests.EmploymentCheckEventTests
+{
+    public abstract class EmploymentCheckEventTestsBase
+    {
+        protected WorkerRole WorkerRole;
+        protected EventTestsRepository EventTestsRepository;
+
+        protected WebApiSubstitute EventsApi => DataAcceptanceTests.EventsApi;
+
+        [SetUp]
+        public void Arrange()
+        {
+            ClearSubstituteApis();
+            StartWorkerRole();
+            SetupDatabase();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Task.Run(() => WorkerRole?.OnStop());
+            WorkerRole = null;
+        }
+
+        private void SetupDatabase()
+        {
+            EventTestsRepository = new EventTestsRepository(DataAcceptanceTests.Config.DatabaseConnectionString);
+            EventTestsRepository.DeleteEmploymentChecks().Wait();
+            EventTestsRepository.DeleteFailedEvents().Wait();
+            EventTestsRepository.StoreLastProcessedEventId("EmploymentCheckCompleteEvent", 2).Wait();
+        }
+
+        private void StartWorkerRole()
+        {
+            WorkerRole = new WorkerRole();
+            WorkerRole.OnStart();
+        }
+
+        private void ClearSubstituteApis()
+        {
+            DataAcceptanceTests.ClearApiSetup();
+        }
+    }
+}
