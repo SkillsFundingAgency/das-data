9adaf349161c1f31c998a67cc760544607bf448c
Michael Young <mick.young1981@googlemail.com>
2017-12-29 08:00:06 +0000

    Added database table

---
+++ src/SFA.DAS.Data.Database/Tables/DAS_EmploymentCheck.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [Data_Load].[DAS_EmploymentCheck]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [NationalInsuranceNumber] NVARCHAR(9) NOT NULL,
+	[Uln] BIGINT NOT NULL,
+	[EmployerAccountId] BIGINT NOT NULL,
+	[Ukprn] BIGINT NOT NULL,
+	[CheckDate] DATE NOT NULL,
+	[CheckPassed] BIT NOT NULL,
+	[IsLatest] BIT NOT NULL DEFAULT 0
+)
+GO
+CREATE INDEX [IX_EmploymentCheck_Uln] ON [Data_Load].[DAS_EmploymentCheck] ([Uln], [IsLatest])
+GO
+CREATE INDEX [IX_EmploymentCheck_CheckPassed] ON [Data_Load].[DAS_EmploymentCheck] ([IsLatest], [CheckPassed])
