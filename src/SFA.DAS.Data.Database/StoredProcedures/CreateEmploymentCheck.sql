41308854b9d25247e57156afd8c800416805ea98
Michael Young <mick.young1981@googlemail.com>
2017-12-29 08:04:59 +0000

    Added stored proc to create employment check records

---
+++ src/SFA.DAS.Data.Database/StoredProcedures/CreateEmploymentCheck.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [Data_Load].[CreateEmploymentCheck]
+	@nationalInsuranceNumber NVARCHAR(9),
+	@uln BIGINT,
+	@employerAccountId BIGINT,
+	@ukprn BIGINT,
+	@checkDate DATE,
+	@checkPassed BIT
+AS
+
+	UPDATE [Data_Load].[DAS_EmploymentCheck] SET IsLatest = 0 WHERE Uln = @uln
+
+	INSERT INTO [Data_Load].DAS_EmploymentCheck 
+	(
+		NationalInsuranceNumber,
+		Uln,
+		EmployerAccountId,
+		Ukprn,
+		CheckDate,
+		CheckPassed,
+		IsLatest
+	)
+	VALUES
+	(
+		@nationalInsuranceNumber,
+		@uln,
+		@employerAccountId,
+		@ukprn,
+		@checkDate,
+		@checkPassed,
+		1
+	)
