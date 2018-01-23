060b060562ba6b5d13167e503c280569d5c97458
David Gouge <davegouge@googlemail.com>
2018-01-23 08:54:58 +0000

    Added Levy Snapshot Stored Proc

---
+++ src/SFA.DAS.Data.Database/StoredProcedures/LevySnapshot.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [Data_Load].[LevySnapshot]
+AS
+	DECLARE @Sql nvarchar(500);
+	SET @Sql = 'SELECT *
+		INTO [Data_Load].[DAS_LevyDeclarations_Snapshot_'+CONVERT(VARCHAR(10), GETDATE(), 112) +']
+		FROM [Data_Load].[DAS_LevyDeclarations]'
+
+	EXEC(@Sql)
+
+RETURN 0
