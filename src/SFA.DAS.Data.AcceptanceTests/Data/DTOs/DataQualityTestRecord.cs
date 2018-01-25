d2c2fdf6347fdddc1d9e2b3a6eeaf4ff916661cf
David Gouge <davegouge@googlemail.com>
2018-01-25 15:45:56 +0000

    Starting tests for Data Quality.

---
+++ src/SFA.DAS.Data.AcceptanceTests/Data/DTOs/DataQualityTestRecord.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace SFA.DAS.Data.AcceptanceTests.Data.DTOs
+{
+    [Table("[HMRC].[Configuration_Data_Quality_Tests]")]
+    public class DataQualityTestRecord
+    {
+        public string ColumnName { get; set; }
+        public bool ColumnNullable { get; set; }
+        public string ColumnType { get; set; }
+        public int ColumnLength { get; set; }
+        public bool RunColumnTests { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Data.AcceptanceTests/HmrcDataLoadTests/WhenDataLoadIsExecutedWithAStringLengthTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Data.AcceptanceTests.Data.DTOs;
+
+namespace SFA.DAS.Data.AcceptanceTests.HmrcDataLoadTests
+{
+    [TestFixture]
+    public class WhenDataLoadIsExecutedWithAStringLengthTest : HmrcDataLoadTestsBase
+    {
+        [Test]
+        public void ThenValidationFailuresAreLoggedIntoConfigurationDataQualityTests()
+        {
+            InsertPendingLoadControl();
+
+            HmrcDataTestsRepository.InsertIntoStaging(new DataStagingRecord{SchemePAYERef = "123456789"}).Wait();
+
+            HmrcDataTestsRepository.InsertIntoDataQualityTests(new DataQualityTestRecord
+            {
+                ColumnName = "SchemePAYERef",
+                ColumnNullable = true,
+                ColumnType = "NVARCHAR",
+                ColumnLength = 5,
+                RunColumnTests = true
+            }).Wait();
+
+            HmrcDataTestsRepository.ExecuteLoadData().Wait();
+
+            var qualityLogs = HmrcDataTestsRepository.GetQualityLogs().Result.ToList();
+
+            qualityLogs.Count().Should().Be(1);
+            qualityLogs.First().ColumnName.Should().Be("SchemePAYERef");
+            qualityLogs.First().ErrorMessage.Should().Be("String length exceeds Specification Actual: 9 Against spec size of: 5");
+            //qualityLogs.First().ShouldBeEquivalentTo(new QualityLog(){ColumnName = "SchemePAYERef", ErrorMessage = "String length exceeds Specification Actual: 9 Against spec size of: 5", TestName = "String Length"});
+        }
+    }
+}
