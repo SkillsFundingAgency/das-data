3237aa988eb95d5dfb60538b573e71ab76193dfc
Michael Young <mick.young1981@googlemail.com>
2018-02-09 07:57:52 +0000

    Added employment check view

---
+++ src/SFA.DAS.Data.Database/Views/DAS_EmploymentCheck.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [Data_Pub].[DAS_EmploymentCheck]
+AS 
+SELECT
+	[Id],
+    [NationalInsuranceNumber],
+	[Uln],
+	[EmployerAccountId],
+	[Ukprn],
+	[CheckDate],
+	[CheckPassed],
+	[IsLatest] AS Flag_Latest,
+	CASE 
+		WHEN CheckPassed = 0 AND EmployerAccountId = 0 THEN 'Submitted UKPRN does not match Commitment' 
+		WHEN CheckPassed = 0 AND EmployerAccountId	<> 0 THEN 'HMRC did not return successful check' 
+		ELSE NULL 
+	END AS CheckFailureReason
+FROM [Data_Load].[DAS_EmploymentCheck]
