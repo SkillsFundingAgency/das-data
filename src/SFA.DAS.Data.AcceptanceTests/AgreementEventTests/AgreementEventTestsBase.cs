734b8cb20008a65c3f34622acf93f0ca13e76b4a
steven scaife <Steven.Scaife@digital.education.gov.uk>
2018-02-14 16:51:00 +0000

    removed code duplication among base event test classes and added factory method for the SetupDatabase implementation

--- src/SFA.DAS.Data.AcceptanceTests/AgreementEventTests/AgreementEventTestsBase.cs
+++ src/SFA.DAS.Data.AcceptanceTests/AgreementEventTests/AgreementEventTestsBase.cs
@@ -6,48 +6,17 @@ using SFA.DAS.Data.Worker;
 
 namespace SFA.DAS.Data.AcceptanceTests.AgreementEventTests
 {
-    public abstract class AgreementEventTestsBase
+    public abstract class AgreementEventTestsBase : EventTestBase
     {
-        protected WorkerRole WorkerRole;
-        protected EventTestsRepository EventTestsRepository;
-
         protected WebApiSubstitute EventsApi => DataAcceptanceTests.EventsApi;
         protected WebApiSubstitute AgreementsApi => DataAcceptanceTests.AgreementEventsApi;
 
-        protected abstract string EventName { get; }
-
-        [SetUp]
-        public void Arrange()
-        {
-            ClearSubstituteApis();
-            StartWorkerRole();
-            SetupDatabase();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            Task.Run(() => WorkerRole?.OnStop());
-            WorkerRole = null;
-        }
-
-        private void SetupDatabase()
+        protected override void SetupDatabase()
         {
             EventTestsRepository = new EventTestsRepository(DataAcceptanceTests.Config.DatabaseConnectionString);
             EventTestsRepository.DeleteProviders().Wait();
             EventTestsRepository.DeleteFailedEvents().Wait();
             EventTestsRepository.StoreLastProcessedEventId(EventName, 2).Wait();
-        }
-
-        private void StartWorkerRole()
-        {
-            WorkerRole = new WorkerRole();
-            WorkerRole.OnStart();
-        }
-
-        private void ClearSubstituteApis()
-        {
-            DataAcceptanceTests.ClearApiSetup();
         }
     }
 }
---
+++ src/SFA.DAS.Data.AcceptanceTests/EventTestBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.Data.AcceptanceTests.Data;
+using SFA.DAS.Data.Worker;
+
+namespace SFA.DAS.Data.AcceptanceTests
+{
+    public abstract class EventTestBase
+    {
+        protected WorkerRole WorkerRole;
+        protected EventTestsRepository EventTestsRepository;
+
+        protected abstract string EventName { get; }
+
+        [SetUp]
+        public void Arrange()
+        {
+            ClearSubstituteApis();
+            StartWorkerRole();
+            SetupDatabase();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Task.Run(() => WorkerRole?.OnStop());
+            WorkerRole = null;
+        }
+
+        protected abstract void SetupDatabase();
+
+        private void StartWorkerRole()
+        {
+            WorkerRole = new WorkerRole();
+            WorkerRole.OnStart();
+        }
+
+        private void ClearSubstituteApis()
+        {
+            DataAcceptanceTests.ClearApiSetup();
+        }
+    }
+}
