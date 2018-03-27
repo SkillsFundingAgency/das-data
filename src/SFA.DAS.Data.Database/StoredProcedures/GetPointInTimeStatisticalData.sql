7cbf8f198c5f7a16066924bc4f58d9fe1cfc9fc9
steven scaife <Steven.Scaife@digital.education.gov.uk>
2018-03-27 16:04:01 +0100

    added stored proc that shows the latest point in time statistics

---
+++ src/SFA.DAS.Data.Database/StoredProcedures/GetPointInTimeStatisticalData.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [Data_Load].[GetPointInTimeStatisticalData]
+AS
+
+SELECT DataType, CheckedDateTime, SourceSystemCount, RdsCount
+FROM [Data_Load].[DAS_ConsistencyCheck]
+WHERE DataType IN 
+	(
+		SELECT DISTINCT DataType FROM [Data_Load].[DAS_ConsistencyCheck]
+	) 
+	AND DATEADD(HOUR, DATEDIFF(HOUR, 0, CheckedDateTime), 0) = 
+	(
+		SELECT MAX(DATEADD(HOUR, DATEDIFF(HOUR, 0, CheckedDateTime), 0))
+		FROM [Data_Load].[DAS_ConsistencyCheck]
+	)
