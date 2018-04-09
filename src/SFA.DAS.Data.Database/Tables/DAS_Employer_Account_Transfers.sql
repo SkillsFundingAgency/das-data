6d462182842c7348e47d889c68ad924a0c34ddc3
Matt Derry <matt@goodoldmatt.com>
2018-04-09 13:50:52 +0000

    adds new transfer tables to database schema

---
+++ src/SFA.DAS.Data.Database/Tables/DAS_Employer_Account_Transfers.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Data_Load].[DAS_Employer_Account_Transfers]
+(
+	[SenderAccountId] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [ReceiverAccountId] BIGINT NOT NULL, 
+    [RelationshipStatus] NVARCHAR(10) NOT NULL, 
+    [SenderUserId] BIGINT NOT NULL, 
+    [ApproverUserId] BIGINT NOT NULL, 
+    [RejectorUserId] BIGINT NOT NULL DEFAULT (GETDATE()), 
+    [Required Payment ID] UNIQUEIDENTIFIER NOT NULL, 
+    [ColectionPeriodName] NCHAR(10) NOT NULL
+)
+GO
---
+++ src/SFA.DAS.Data.Database/Tables/DAS_Employer_Transfer_Relationships.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Data_Load].[DAS_Employer_Transfer_Relationships]
+(
+	[Sender Account Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [Receiver Account Id] BIGINT NOT NULL, 
+    [Relationship Status] NVARCHAR(10) NOT NULL, 
+    [Sender User Id] BIGINT NOT NULL, 
+    [Approver User Id] BIGINT NOT NULL, 
+    [Rejector User Id] BIGINT NOT NULL DEFAULT (GETDATE()) 
+)
+GO
