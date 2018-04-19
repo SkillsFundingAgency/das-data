1ec0a947499f96599023eb9378a95cdc3a406b41
Maxim Beliy <maximbel@gmail.com>
2018-04-19 12:27:16 +0100

    Indexes and UpdateDateTime field added to the new table

--- src/SFA.DAS.Data.Database/Tables/DAS_Employer_Account_Transfers.sql
+++ src/SFA.DAS.Data.Database/Tables/DAS_Employer_Account_Transfers.sql
@@ -8,6 +8,13 @@
 	[Amount]	DECIMAL(18, 5) NULL,
 	[Type] NVARCHAR(50) NOT NULL,
 	[TransferDate] DATE NOT NULL,
-    [ColectionPeriodName] NCHAR(10) NOT NULL 
+    [ColectionPeriodName] NCHAR(10) NOT NULL, 
+    [UpdateDateTime] DATETIME NOT NULL DEFAULT (GETDATE()) 
 )
+GO
+
+CREATE INDEX [IX_DAS_Employer_Account_Transfers_SenderAccountId] ON [Data_Load].[DAS_Employer_Account_Transfers] ([SenderAccountId])
+GO
+
+CREATE INDEX [IX_DAS_Employer_Account_Transfers_ReceiverAccountId] ON [Data_Load].[DAS_Employer_Account_Transfers] ([ReceiverAccountId])
 GO
