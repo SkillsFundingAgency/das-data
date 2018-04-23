733e5a061de43b4c6687b29872acdf65b3c262dc
desderry <matt@goodoldmatt.co.uk>
2018-04-23 09:20:52 +0000

    re-applys outer apply to commitment view for legal entities (#106)

--- src/SFA.DAS.Data.Database/Views/DAS_Commitments.sql
+++ src/SFA.DAS.Data.Database/Views/DAS_Commitments.sql
@@ -102,7 +102,7 @@ SELECT [C].[ID]
 		 LEFT JOIN [Data_Load].[DAS_Employer_Accounts] EAA ON EAA.AccountId = [C].[EmployerAccountID] AND EAA.IsLatest = 1
 
 		 ---- Join Legal Entity to get Legal_Entity_ID
-		 LEFT JOIN (SELECT 
+		 OUTER APPLY (SELECT 
                     DISTINCT TOP 1
                       ELE.DasAccountId
                     , ELE.COde AS [LegalEntityNumber]
@@ -114,7 +114,11 @@ SELECT [C].[ID]
                     Data_Load.DAS_Employer_LegalEntities AS ELE
                WHERE
                    IsLatest = 1
-               ) AS ELE ON EAA.DasAccountId = ELE.DasAccountId
+				   AND ELE.DasAccountId = EAA.DasAccountId
+                   AND ELE.Code = [C].LegalEntityCode
+                   AND ELE.Name = [C].LegalEntityName   
+                   AND REPLACE(ELE.Source,' ','') = [C].[LegalEntityOrganisationType]
+               ) AS ELE 
 
 		  LEFT JOIN (SELECT P.ApprenticeshipId AS CommitmentId
 					  , SUM(P.Amount) AS TotalAmount
