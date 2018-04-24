8143850d141ee0e5f7dc65a57b8d2bf27c2d10b8
Ewan Noble <ewan.noble@education.gov.uk>
2018-04-24 11:25:22 +0100

    Point dotnet restore to correct project

--- src/CustomTargets.fsx
+++ src/CustomTargets.fsx
@@ -3,8 +3,5 @@ open Fake
 Target "Dotnet Restore" (fun _ ->
     DotNetCli.Restore(fun p ->
         { p with
-                Project = ".\\SFA.DAS.Account.Api.Client" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\SFA.DAS.EAS.Account.Api.Types" })
+                Project = ".\\SFA.DAS.Data.Functions" })
 )
