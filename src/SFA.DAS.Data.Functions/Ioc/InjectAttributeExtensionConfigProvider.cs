f71c4de0d4cd736c0d2142048d12814a9fb8ec09
suniara <35219355+suniara@users.noreply.github.com>
2018-04-24 15:01:03 +0100

    Azure functions with IOC (#109)
    * Azure functions with IOC
    
    * Removed Mediatr registration and tidy up StructureMapObjectResolver
    
    * Removed Mediatr package

--- src/SFA.DAS.Data.Functions/Ioc/InjectAttributeExtensionConfigProvider.cs
+++ src/SFA.DAS.Data.Functions/Ioc/InjectAttributeExtensionConfigProvider.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host.Config;
 
 namespace SFA.DAS.Data.Functions.Ioc
 {
-    public class InjectAttributeExtensionConfigProvider : IExtensionConfigProvider 
+    public class InjectAttributeExtensionConfigProvider : IExtensionConfigProvider
     {
         private readonly InjectAttributeBindingProvider _bindingProvider;
         public InjectAttributeExtensionConfigProvider()
@@ -16,7 +12,7 @@ namespace SFA.DAS.Data.Functions.Ioc
         }
         public void Initialize(ExtensionConfigContext context)
         {
-            context.AddBindingRule<InjectAttribute>().Bind(_bindingProvider);
+            context.Config.RegisterBindingExtensions(_bindingProvider);
         }
     }
 }
