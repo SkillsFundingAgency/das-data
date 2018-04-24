80a8edffa947017307577b21b6225e4577f44ece
Matt Derry <matt@goodoldmatt.com>
2018-04-24 09:21:24 +0000

    Revert "updates payment table to include transfer fields"
    This reverts commit 1e96fd2cfee3c0143b91ca61b69c32acb1e78e8a.

--- src/SFA.DAS.Data.Database/Tables/DAS_Payments.sql
+++ src/SFA.DAS.Data.Database/Tables/DAS_Payments.sql
@@ -21,8 +21,7 @@
 	[ProgrammeType]	INT NULL,
 	[PathwayCode]	INT NULL,
 	[ContractType]	NVARCHAR(50) NULL, 
-    [UpdateDateTime] DATETIME NOT NULL DEFAULT (GETDATE()), 
-    [FundingAccountId] BIGINT NULL
+    [UpdateDateTime] DATETIME NOT NULL DEFAULT (GETDATE())
 )
 GO
 CREATE INDEX [IX_Payment_FundingSource] ON [Data_Load].[DAS_Payments] ([FundingSource])
