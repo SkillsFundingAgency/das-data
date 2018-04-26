3f9d4b0dc97cefe9851671f6b546b37c7624dca0
Maxim Beliy <maximbel@gmail.com>
2018-04-26 14:32:50 +0100

    payments and transfers changes to the DB

---
+++ src/SFA.DAS.Data.Database/StoredProcedures/SaveTransfer.sql
@@ -0,0 +1,71 @@
+﻿CREATE PROCEDURE [Data_Load].[SaveTransfers]
+	@transfers [Data_Load].[TransferEntity] readonly
+AS
+BEGIN
+	INSERT INTO [Data_Load].[DAS_Employer_Account_Transfers]
+	(
+		[Id],
+		[SenderAccountId],
+		[ReceiverAccountId],
+		[RequiredPaymentId],
+		[CommitmentId],
+		[Amount],
+		[Type],
+		[TransferDate],
+		[CollectionPeriodName]
+	)
+	SELECT
+		[Id],
+		[SenderAccountId],
+		[ReceiverAccountId],
+		[RequiredPaymentId],
+		[CommitmentId],
+		[Amount],
+		[Type],
+		[TransferDate],
+		[CollectionPeriodName]
+	FROM
+		@transfers t
+	WHERE
+		NOT EXISTS(SELECT 1 FROM [Data_Load].[DAS_Employer_Account_Transfers] t2 WHERE t2.Id = t.Id)
+
+--ECREATE PROCEDURE [Data_Load].[SaveTransfer]
+--	@id UNIQUEIDENTIFIER,
+--	@senderAccountId BIGINT, 
+--    @receiverAccountId BIGINT, 
+--    @requiredPaymentId UNIQUEIDENTIFIER, 
+--	@commitmentId BIGINT,
+--	@amount DECIMAL(18, 5),
+--	@type NVARCHAR(50),
+--	@transferDate DATE,
+--    @collectionPeriodName NCHAR(10)
+--AS
+--BEGIN
+--	IF NOT EXISTS(SELECT TOP 1 1 FROM [Data_Load].DAS_Payments WHERE Id = @Id)
+--		INSERT INTO [Data_Load].[DAS_Employer_Account_Transfers]
+--		(
+--			[Id],
+--			[SenderAccountId],
+--			[ReceiverAccountId],
+--			[RequiredPaymentId],
+--			[CommitmentId],
+--			[Amount],
+--			[Type],
+--			[TransferDate],
+--			[CollectionPeriodName]
+--		)
+--		VALUES
+--		(
+--			@id,
+--			@senderAccountId, 
+--			@receiverAccountId, 
+--			@requiredPaymentId, 
+--			@commitmentId,
+--			@amount,
+--			@type,
+--			@transferDate,
+--			@collectionPeriodName
+--		)
+
+
+END
---
+++ src/SFA.DAS.Data.Database/Types/TransferEntity.sql
@@ -0,0 +1,11 @@
+﻿CREATE TYPE [Data_Load].[TransferEntity] AS TABLE (
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[SenderAccountId] BIGINT NOT NULL, 
+    [ReceiverAccountId] BIGINT NOT NULL, 
+    [RequiredPaymentId] UNIQUEIDENTIFIER NOT NULL, 
+	[CommitmentId] BIGINT NOT NULL,
+	[Amount]	DECIMAL(18, 5) NULL,
+	[Type] NVARCHAR(50) NOT NULL,
+	[TransferDate] DATE NOT NULL,
+    [CollectionPeriodName] NVARCHAR(10) NOT NULL
+);
