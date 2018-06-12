112daaa5f6aff6493f9a2b50de7692b9d0e17511
Mahinder Suniara <mahinder.suniara@btinternet.com>
2018-06-12 13:41:22 +0100

    Acceptance tests for Commitments statistics

---
+++ src/SFA.DAS.Data.AcceptanceTests/Functions/Statistics/Commitments/WhenIGetCommitmentsStatistics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.Data.Domain.Models.Statistics.Commitments;
+
+namespace SFA.DAS.Data.AcceptanceTests.Functions.Statistics.Commitments
+{
+    [TestFixture]
+    public class WhenIGetCommitmentsStatistics : CommitmentsTestBase
+    {
+        [Test]
+        public async Task ThenSaveCommitmentsStatistics()
+        {
+            await SetupDatabase();
+
+            var commitmentsExternalModel = new CommitmentsExternalModel()
+            {
+                TotalCohorts = 1, TotalApprenticeships = 1, ActiveApprenticeships = 1
+            };
+            CommitmentsStatisticsHandlerMock.Setup(x => x.Handle()).Returns(Task.FromResult(commitmentsExternalModel));
+
+            await DAS.Data.Functions.Statistics.GetCommitmentStatisticsFunction.Run(null, Log, StatisticsService);
+
+            var databaseAsExpected = TestHelper.ConditionMet(IsDatabaseInExpectedState, TimeSpan.FromSeconds(60));
+
+            Assert.IsTrue(databaseAsExpected);
+        }
+
+        private async Task<bool> IsDatabaseInExpectedState()
+        {
+            var commitmentsStatisticsCount = await StatisticsTestsRepository.GetNumberOfLatestActiveApprenticeships();
+            commitmentsStatisticsCount += await StatisticsTestsRepository.GetNumberOfLatestTotalApprenticeships();
+            commitmentsStatisticsCount += await StatisticsTestsRepository.GetNumberOfLatestTotalCohorts();
+
+            return commitmentsStatisticsCount == 3;
+        }
+    }
+}
--- src/SFA.DAS.Data.Functions/Statistics/GetCommitmentStatisticsFunction.cs
+++ src/SFA.DAS.Data.Functions/Statistics/GetCommitmentStatisticsFunction.cs
@@ -1,6 +1,5 @@
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using SFA.DAS.Data.Application.Messages;
 using SFA.DAS.Data.Domain.Interfaces;
 using SFA.DAS.Data.Functions.Ioc;
 using SFA.DAS.NLog.Logger;
@@ -10,8 +9,6 @@ namespace SFA.DAS.Data.Functions.Statistics
     public static class GetCommitmentStatisticsFunction
     {
         [FunctionName("GetCommitmentStatisticsFunction")]
-        [Disable]
-       // [Disable]
         public static async Task Run([TimerTrigger("*/15 * * * * *")] TimerInfo myTimer, [Inject] ILog log,
         [Inject] IStatisticsService statsService)
         {
