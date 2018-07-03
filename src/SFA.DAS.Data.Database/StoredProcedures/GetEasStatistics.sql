09f9109698d4ea329c791f36a527f9ca99c5059e
Mahinder Suniara <mahinder.suniara@btinternet.com>
2018-07-03 11:55:36 +0100

    Updated TotalPayments & TotalLegalEntities to reflect changes in eas

--- src/SFA.DAS.Data.Database/StoredProcedures/GetEasStatistics.sql
+++ src/SFA.DAS.Data.Database/StoredProcedures/GetEasStatistics.sql
@@ -9,12 +9,10 @@ SELECT (
 (
 	SELECT COUNT(Id) AS TotalPayments 
 	FROM [Data_Load].[DAS_Payments]
-	WHERE [CollectionYear] = YEAR(GETDATE())
 ) AS TotalPayments ,
 (
 	SELECT COUNT(Id)
 	FROM [Data_Load].[DAS_Employer_LegalEntities]
-	WHERE IsLatest = 1 AND [Status]='active'
 ) AS TotalLegalEntities,
 (
 	SELECT COUNT(Id) 
