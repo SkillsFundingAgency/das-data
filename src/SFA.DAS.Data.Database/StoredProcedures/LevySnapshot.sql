e2b5c0ab3002f8e5795cde423e6b79e5b4292556
Mike Wild <mike.wild@digital.education.gov.uk>
2018-08-28 15:40:51 +0100

    Run levy declaration snapshot proc at the end of HMRC_Load, and add an entry to Process_Log when done. Make levy declarations snapshot.proc re-runnable by dropping the target table if it exists.

--- src/SFA.DAS.Data.Database/StoredProcedures/LevySnapshot.sql
+++ src/SFA.DAS.Data.Database/StoredProcedures/LevySnapshot.sql
@@ -1,8 +1,15 @@
 ﻿CREATE PROCEDURE [Data_Load].[LevySnapshot]
 AS
 	DECLARE @Sql nvarchar(500);
+	DECLARE @TableName nvarchar(100);
+
+	SET @TableName = '[Data_Load].[DAS_LevyDeclarations_Snapshot_' + CONVERT(VARCHAR(10), GETDATE(), 112) +']'
+	
+	IF OBJECT_ID(@TableName, N'U') IS NOT NULL
+		EXEC('DROP TABLE ' + @TableName)
+
 	SET @Sql = 'SELECT *
-		INTO [Data_Load].[DAS_LevyDeclarations_Snapshot_'+CONVERT(VARCHAR(10), GETDATE(), 112) +']
+		INTO ' + @TableName + '
 		FROM [Data_Load].[DAS_LevyDeclarations]'
 
 	EXEC(@Sql)
