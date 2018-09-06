e1899065ceebb67626c94a3a10d2bfcd4bf85337
Mike Wild <40885416+mikeymonster@users.noreply.github.com>
2018-09-06 13:56:56 +0100

    Add schema and tables for data lock reporting in RDS. (#142)

---
+++ src/SFA.DAS.Data.Database/Tables/DAS_DataLocks.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [Data_Lock].[DAS_DataLocks]
+(
+	[Id] [bigint] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Collection] NVARCHAR(10) NOT NULL,
+	[Ukprn] BIGINT NOT NULL,
+	[LearnRefNumber] NVARCHAR(12) NOT NULL,
+	[ULN] [BIGINT] NOT NULL,
+	[AimSeqNumber] BIGINT NOT NULL,
+	[RuleId] [NVARCHAR](10) NOT NULL,
+	[CollectionPeriodName] NVARCHAR(10) NOT NULL,
+	[CollectionPeriodMonth] INT NOT NULL,
+	[CollectionPeriodYear] INT NOT NULL,
+	[LastSubmission] DATETIME NULL,
+	[TNP] BIGINT NOT NULL
+)
