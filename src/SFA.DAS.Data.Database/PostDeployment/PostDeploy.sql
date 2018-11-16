352289db54555a4face9db35d6dba65fbfedacd7
Mike Wild <mike.wild@digital.education.gov.uk>
2018-11-16 08:24:53 +0000

    Add one more obsolete Data_Lock table to cleanup in post deployment script (looks like an old table name that only made it into AT environment, but causes script to fail in build)

--- src/SFA.DAS.Data.Database/PostDeployment/PostDeploy.sql
+++ src/SFA.DAS.Data.Database/PostDeployment/PostDeploy.sql
@@ -246,6 +246,8 @@ if exists (select * from sys.tables where name = 'DAS_ValidAims' and schema_id =
 	DROP TABLE [Data_Lock].[DAS_ValidAims]
 if exists (select * from sys.tables where name = 'DAS_DataLocks' and schema_id =  SCHEMA_ID('Data_Lock'))
 	DROP TABLE [Data_Lock].[DAS_DataLocks]
+if exists (select * from sys.tables where name = 'DAS_DataLock' and schema_id =  SCHEMA_ID('Data_Lock'))
+	DROP TABLE [Data_Lock].[DAS_DataLock]
 
 IF EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'Data_Lock' )
 	DROP SCHEMA [Data_Lock]
