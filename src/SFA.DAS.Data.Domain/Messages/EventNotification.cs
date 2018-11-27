369481a9bbb4986a930aefb1b8e509fe6be22226
Neil Winstanley <Winsto@users.noreply.github.com>
2018-11-27 18:35:53 +0000

    Add a type to tie together an NServicebus Event and a Mediatr Notification

---
+++ src/SFA.DAS.Data.Domain/Messages/EventNotification.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SFA.DAS.NServiceBus;
+
+namespace SFA.DAS.Data.Domain.Messages
+{
+    public abstract class EventNotification<EventType> : INotification
+        where EventType : Event
+    {
+        public EventNotification(EventType eventPayload)
+        {
+            EventPayload = eventPayload;
+        }
+
+        public EventType EventPayload { get; }
+    }
+}
