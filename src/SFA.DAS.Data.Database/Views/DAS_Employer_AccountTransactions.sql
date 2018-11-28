badf640dc2518946e5190f7f215f2a1cf3c17361
Mike Wild <mike.wild@digital.education.gov.uk>
2018-11-28 12:07:32 +0000

    Add transfers + CollectionPeriodName to DAS_Employer_AccountTransactions view

--- src/SFA.DAS.Data.Database/Views/DAS_Employer_AccountTransactions.sql
+++ src/SFA.DAS.Data.Database/Views/DAS_Employer_AccountTransactions.sql
@@ -9,6 +9,7 @@
 		  ,    LD.PayrollYear
 		  ,    NULL AS CollectionMonth
 		  ,    NULL AS CollectionYear
+		  ,	   NULL As CollectionPeriodName	
           ,    'IN Levy Money from HMRC' AS TransactionType
           ,    ROUND((LD.LevyDeclaredInMonth * LD.EnglishFraction),2) AS Amount
           FROM Data_Load.DAS_LevyDeclarations AS LD
@@ -23,12 +24,13 @@
 			   ,    LD.PayrollYear
 			   ,    NULL AS CollectionMonth
 			   ,    NULL AS CollectionYear
+			   ,	NULL As CollectionPeriodName	
                ,    'IN Levy Top Up Amount' AS TransactionType
                ,    ROUND(LD.[TopupAmount],2) AS Amount
           FROM Data_Load.DAS_LevyDeclarations AS LD
-          WHERE LD.IsLatest = 1
-       UNION ALL
-     --- Payments 
+		  WHERE LD.IsLatest = 1
+    UNION ALL
+      -- Payments 
           SELECT 
                     [EA].[DasAccountId] AS DasAccountId
                ,    PS.UpdateDateTime AS CreatedDate
@@ -37,11 +39,92 @@
 			   ,	NULL as PayrollYear
 			   ,    CollectionMonth
 			   ,    CollectionYear
+			   ,	CollectionPeriodName
                ,    'OUT '+[TransactionType]  AS TransactionType
                ,    ROUND((PS.Amount*-1),2) AS Amount -- Made negative as Payment
           FROM Data_Load.DAS_Payments AS PS
-          ---- DAS Account Name
-               LEFT JOIN [Data_Load].[DAS_Employer_Accounts] EA ON EA.AccountId = [PS].[EmployerAccountId] AND EA.IsLatest = 1
+          -- DAS Account Name
+               LEFT JOIN 
+			   [Data_Load].[DAS_Employer_Accounts] EA 
+			   ON EA.AccountId = [PS].[EmployerAccountId] AND 
+			   EA.IsLatest = 1
           WHERE PS.EmployerAccountId IS NOT NULL -- removes non Levy Activity
                AND  PS.FundingSource = 'Levy'  -- Only Month coming from balances
+               --AND  PS.IsLatest = 1
+
+	 UNION ALL
+
+---TRF Levy Transfer Sent
+
+	SELECT 
+                    [EA].[DasAccountId] AS DasAccountId
+			   --,    EA.[EmployerAccountID]
+               ,    PS.UpdateDateTime AS CreatedDate
+			   ,    NULL as SubmissionDate
+			   ,    NULL as PayrollMonth
+			   ,	NULL as PayrollYear
+			   ,    CollectionMonth
+			   ,    CollectionYear
+			   ,	CollectionPeriodName
+               ,    'TRF '+'Levy Transfer Sent'  AS TransactionType
+               ,    ROUND((PS.Amount*-1),2) AS Amount -- Made negative as Payment
+          FROM Data_Load.DAS_Payments AS PS
+          -- DAS Account Name
+               LEFT JOIN 
+			   [Data_Load].[DAS_Employer_Accounts] EA 
+			   ON EA.AccountId = [PS].[EmployerAccountId] AND 
+			   EA.IsLatest = 1
+          WHERE PS.FundingAccountId IS NOT NULL AND PS.EmployerAccountId IS NOT NULL -- removes non Levy Activity
+               AND  PS.FundingSource = 'LevyTransfer'  -- Only Month coming from balances
+               --AND  PS.IsLatest = 1
+	UNION ALL
+
+---TRF Levy Transfer Recieved
+
+	SELECT 
+                    [EA].[DasAccountId] AS DasAccountId
+               ,    PS.UpdateDateTime AS CreatedDate
+			   ,    NULL AS SubmissionDate
+			   ,    NULL AS PayrollMonth
+			   ,	NULL AS PayrollYear
+			   ,    CollectionMonth
+			   ,    CollectionYear
+			   ,	CollectionPeriodName
+               ,    'TRF '+'Levy Transfer Recieved'  AS TransactionType
+               ,    ROUND((PS.Amount*-1),2) AS Amount -- Made negative as Payment
+          FROM Data_Load.DAS_Payments 
+		  AS PS
+          -- DAS Account Name
+               LEFT JOIN 
+			   [Data_Load].[DAS_Employer_Accounts] EA 
+			   ON EA.AccountId = [PS].[EmployerAccountId] AND 
+			   EA.IsLatest = 1
+          WHERE PS.FundingAccountId IS NOT NULL and PS.EmployerAccountId IS NOT NULL -- removes non Levy Activity
+               AND  PS.FundingSource = 'LevyTransfer'  -- Only Month coming from balances
+               --AND  PS.IsLatest = 1
+
+			    UNION ALL
+
+---Out Learning Levy Transfer
+
+			   SELECT 
+                    [EA].[DasAccountId] AS DasAccountId
+               ,    PS.UpdateDateTime AS CreatedDate
+			   ,    NULL as SubmissionDate
+			   ,    NULL as PayrollMonth
+			   ,	NULL as PayrollYear
+			   ,    CollectionMonth
+			   ,    CollectionYear
+			   ,	CollectionPeriodName
+               ,    'OUT '+'Learning Levy Transfer'  AS TransactionType
+               ,    ROUND((PS.Amount*-1),2) AS Amount -- Made negative as Payment
+          FROM Data_Load.DAS_Payments 
+		  AS PS
+          ---- DAS Account Name
+               LEFT JOIN 
+			   [Data_Load].[DAS_Employer_Accounts] 
+			   EA ON EA.AccountId = [PS].[EmployerAccountId] AND 
+			   EA.IsLatest = 1
+          WHERE PS.EmployerAccountId IS NOT NULL -- removes non Levy Activity
+               AND  PS.FundingSource = 'LevyTransfer'  -- Only Month coming from balances
                --AND  PS.IsLatest = 1
