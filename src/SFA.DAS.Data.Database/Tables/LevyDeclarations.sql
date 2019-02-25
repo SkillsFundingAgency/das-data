56a5b1e0066b0e67777b859e27664877be9b9267
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2019-02-25 11:03:28 +0000

    Remove_Duplicate_HMRC_Tables

--- src/SFA.DAS.Data.Database/Tables/LevyDeclarations.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [HMRC].[LevyDeclarations]
-(
-	[SchemePAYERef] [varchar](14) NOT NULL,
-	[LevyDueYearToDate]	[decimal](38,6) NOT NULL,
-	[TaxPeriodStartYear] [INT] NOT NULL,
-	[TaxPeriodMonth] [INT] NOT NULL
-)
