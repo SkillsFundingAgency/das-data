8be5a6a6caea522336fc84dd8cde468a1b84c8ec
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2019-03-01 12:49:31 +0000

    HMRC Tables Definition Change

--- src/SFA.DAS.Data.Database/Tables/HMRC_EnglishFractions.sql
+++ src/SFA.DAS.Data.Database/Tables/HMRC_EnglishFractions.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [HMRC].[EnglishFractions]
 (
 	[SchemePAYERef] [varchar](14) NOT NULL,
-	[EnglishFractionTransactionScanDate] DATE NOT NULL,
-	[EnglishFraction] [DECIMAL](38,6) NOT NULL
+	[EnglishFractionTransactionScanDate] DATE NULL,
+	[EnglishFraction] [DECIMAL](38,6) NULL
 )
--- src/SFA.DAS.Data.Database/Tables/HMRC_LevyDeclarations.sql
+++ src/SFA.DAS.Data.Database/Tables/HMRC_LevyDeclarations.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [HMRC].[LevyDeclarations]
 (
 	[SchemePAYERef] [varchar](14) NOT NULL,
-	[LevyDueYearToDate]	[decimal](38,6) NOT NULL,
-	[TaxPeriodStartYear] [INT] NOT NULL,
-	[TaxPeriodMonth] [INT] NOT NULL
+	[LevyDueYearToDate]	[decimal](38,6) NULL,
+	[TaxPeriodStartYear] [INT] NULL,
+	[TaxPeriodMonth] [INT] NULL
 )
