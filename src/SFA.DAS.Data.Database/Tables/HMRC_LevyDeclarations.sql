a1198f4015f6cd140e015272177e1c4f793508bd
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-03-13 13:59:33 +0000

    Change HMRC Table Definition Add Load Id

--- src/SFA.DAS.Data.Database/Tables/HMRC_LevyDeclarations.sql
+++ src/SFA.DAS.Data.Database/Tables/HMRC_LevyDeclarations.sql
@@ -1,5 +1,8 @@
 ﻿CREATE TABLE [HMRC].[LevyDeclarations]
-(
+(   
+    LD_ID int identity(1,1),
+	Load_ID INT  default(CAST(CAST(YEAR(GETDATE()) AS VARCHAR)+RIGHT('0' + RTRIM(cast(MONTH(getdate()) as varchar)), 2) +CAST(DAY(GETDATE()) AS VARCHAR) as int)) not null,
+	Created_Date DateTime default(getdate()) not null,
 	[SchemePAYERef] [varchar](14) NOT NULL,
 	[LevyDueYearToDate]	[decimal](38,6) NULL,
 	[TaxPeriodStartYear] [INT] NULL,
