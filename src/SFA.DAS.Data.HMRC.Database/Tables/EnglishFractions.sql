1fd8ac9dd927f63958e7389aeba14d28b07f71e2
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-03-20 15:04:40 +0000

    Add LoadId, Id and Created Date on HMRC Tables

--- src/SFA.DAS.Data.HMRC.Database/Tables/EnglishFractions.sql
+++ src/SFA.DAS.Data.HMRC.Database/Tables/EnglishFractions.sql
@@ -1,5 +1,8 @@
 ﻿CREATE TABLE [HMRC].[EnglishFractions]
-(
+(   
+    EF_ID int identity(1,1) ,
+	Load_ID INT  default(CAST(CAST(YEAR(GETDATE()) AS VARCHAR)+RIGHT('0' + RTRIM(cast(MONTH(getdate()) as varchar)), 2) +CAST(DAY(GETDATE()) AS VARCHAR) as int)) not null,
+	Created_Date DateTime default(getdate()) not null,
 	[SchemePAYERef] [varchar](14) NOT NULL,
 	[EnglishFractionTransactionScanDate] DATE NOT NULL,
 	[EnglishFraction] [DECIMAL](38,6) NOT NULL
