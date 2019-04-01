217d598a7fdc5a19094df603981531b21c9c8378
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-04-01 17:11:56 +0100

    Staging Tables in HMRC Database

---
+++ src/SFA.DAS.Data.HMRC.Database/Tables/Stg_Load_EF.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [HMRC].[Stg_Load_EF]
+(	
+    [SchemePAYERef] [varchar](255)  NULL,
+	[EnglishFractionTransactionScanDate] [varchar](255) NULL,
+	[EnglishFraction] [varchar](255) NULL
+)
+
