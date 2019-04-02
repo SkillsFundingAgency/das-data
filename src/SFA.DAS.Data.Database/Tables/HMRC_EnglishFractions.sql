20cc213e19bc6a58b124cc800322b6239b7f04e6
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-04-02 11:06:45 +0100

    Add New Staging Tables (#161)
    * Add New Staging Tables
    
    * Add Source File Name to HMRC Tables
    
    * Staging Tables in HMRC Database

--- src/SFA.DAS.Data.Database/Tables/HMRC_EnglishFractions.sql
+++ src/SFA.DAS.Data.Database/Tables/HMRC_EnglishFractions.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [HMRC].[EnglishFractions]
 (  
     EF_ID int identity(1,1),
-	Load_ID INT  default(CAST(CAST(YEAR(GETDATE()) AS VARCHAR)+RIGHT('0' + RTRIM(cast(MONTH(getdate()) as varchar)), 2) +CAST(DAY(GETDATE()) AS VARCHAR) as int)) not null,
+	Load_ID INT  default(CAST(CAST(YEAR(GETDATE()) AS VARCHAR)+RIGHT('0' + RTRIM(cast(MONTH(getdate()) as varchar)), 2) +RIGHT('0' +RTRIM(CAST(DAY(GETDATE()) AS VARCHAR)),2) as int)) not null,
 	Created_Date DateTime default(getdate()) not null,
+	[Source_File_Name] varchar(max) null,
 	[SchemePAYERef] [varchar](14) NOT NULL,
 	[EnglishFractionTransactionScanDate] DATE NULL,
 	[EnglishFraction] [DECIMAL](38,6) NULL
---
+++ src/SFA.DAS.Data.Database/Tables/HMRC_Stg_Load_LD.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [HMRC].[Stg_Load_LD]
+(   
+	[SchemePAYERef] [varchar](255)  NULL,
+	[LevyDueYearToDate]	VARCHAR(255) NULL,
+	[TaxPeriodYearMonth] varchar(255) null
+)
